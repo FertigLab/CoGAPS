d9b3c3656c8c3a8c40b5d45b7404e8fb9d29d9f2
e.fertig <e.fertig@bc3139a8-67e5-0310-9ffc-ced21a209358>
2013-03-15 16:21:09 +0000

    Updated gene specific statistic and added plotting function
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/CoGAPS@74388 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/calcPermGS.R
+++ R/calcPermGS.R
@@ -1,49 +1,61 @@
-calcGeneGSStat  <- function(Amean, Asd, GSGenes, numPerm, nullGenes=F) {
 
-   # Compute the gene set statistics of upregulation
-   gsStat <- calcCoGAPSStat(Amean, Asd, data.frame(GSGenes), 
-  			    numPerm=numPerm)
-   gsStats <- -log(gsStat$GSUpreg)
-
-   if (sum(gsStat) < .Machine$double.eps) {
-      return(0.)
-   }
-
-   # get the ranked statistic for targets of the gene set		
-   if (nullGenes) {
-      # genes outside of the set serve as a null distribution
-      ZD <- Amean[setdiff(row.names(Amean), GSGenes),] /
-            Asd[setdiff(row.names(Amean), GSGenes),]
-   } else {
-      # statistics for genes within the set
-      ZD <- Amean[GSGenes,]/Asd[GSGenes,]
-   }
-  
-   # compute the statistic
-   outStats <- apply(sweep(ZD,2,gsStat,FUN="*"),1,sum) / (sum(gsStat))
-   outStats <- outStats / apply(ZD,1,sum) # normalization
-   outStats[which(apply(ZD,1,sum) < .Machine$double.eps)] <- 0.
+calcGeneGSStat  <- function(Amean, Asd, GSGenes, numPerm, Pw=rep(1,ncol(Amean)), nullGenes=F) {
+	gsStat <- calcCoGAPSStat(Amean, Asd, data.frame(GSGenes), 
+							 numPerm=numPerm)
+	gsStat <- gsStat$GSUpreg
 	
-   return(outStats)
+	gsStat <- -log(gsStat)
+  
+  if (!all(is.na(Pw))) {
+    if (length(Pw) != length(gsStat)) {
+      stop('Invalid weighting')
+    }
+    gsStat <- gsStat*Pw
+  }
+		
+	if (nullGenes) {
+		ZD <- Amean[setdiff(row.names(Amean), GSGenes),] /
+     	Asd[setdiff(row.names(Amean), GSGenes),]
+	} else {
+		ZD <- Amean[GSGenes,]/Asd[GSGenes,]
+	}
+	outStats <- apply(sweep(ZD,2,gsStat,FUN="*"),1,sum) / (sum(gsStat))
+	
+	outStats <- outStats / apply(ZD,1,sum)
+	
+	outStats[which(apply(ZD,1,sum) < .Machine$double.eps)] <- 0.
+	
+	
+	if (sum(gsStat) < .Machine$double.eps) {
+		return(0.)
+	}
+	
+	return(outStats)
 	
 }
 
 
-computeGeneGSProb <- function(Amean, Asd, GSGenes, numPerm=500) {
-
-   # get the gene membership statistic for genes within the set
-   geneGSStat <- calcGeneGSStat(Amean=Amean, Asd=Asd,
-				GSGenes=GSGenes, numPerm=numPerm)
+computeGeneGSProb <- function(Amean, Asd, GSGenes, Pw=rep(1,ncol(Amean)),
+                              numPerm=500, PwNull=F) {
+	geneGSStat <- calcGeneGSStat(Amean=Amean, Asd=Asd, Pw=Pw,
+								 GSGenes=GSGenes, numPerm=numPerm)
 	
-   # get the gene membership statistic for null genes
-   nullGSStat <- calcGeneGSStat(Amean=Amean, Asd=Asd,
-			        GSGenes=GSGenes, numPerm=numPerm,
-				nullGenes=T)
 	
-   # perform the permutation test to get the p-values
-   finalStats <- sapply(GSGenes,
-			function(x){length(which(nullGSStat > geneGSStat[x])) /
-                                    length(nullGSStat)})
 	
-   return(finalStats)
+	if (PwNull) {
+	  permGSStat <- calcGeneGSStat(Amean=Amean, Asd=Asd,
+								  GSGenes=GSGenes, numPerm=numPerm, Pw=Pw,
+								  nullGenes=T)
+	} else {
+      permGSStat <- calcGeneGSStat(Amean=Amean, Asd=Asd,
+								  GSGenes=GSGenes, numPerm=numPerm, 
+								  nullGenes=T)
+	}
+	
+	finalStats <- sapply(GSGenes,
+						 function(x){length(which(permGSStat > geneGSStat[x])) / length(permGSStat)})
+	
+	return(finalStats)
+	
+	
 }
