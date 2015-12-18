377e7d65ed11ece1d39c69134a69966d1422d0a7
Jacob Carey <jacobcvt12@gmail.com>
2015-12-18 15:59:50 -0500

    Roxygen-ify NAMESPACE

---
+++ R/package.R
@@ -0,0 +1,14 @@
+#' Coordinated Gene Activity in Pattern Sets
+#'
+#' @docType package
+#' @name CoGAPS
+#' @importFrom Rcpp evalCpp
+#' @importFrom gplots heatmap.2
+#' @importFrom stats variable.names sd update heatmap runif
+#' @importFrom graphics matplot title
+#' @importFrom grDevices dev.new dev.off pdf
+#' @importFrom methods is
+#' @importFrom utils read.table write.table
+#' @importFrom RColorBrewer brewer.pal
+#' @useDynLib CoGAPS
+NULL
