a57e34de2e2e21b9b8c55ccf47aa1efa2f1eb41d
Jacob Carey <jacobcvt12@gmail.com>
2015-12-28 10:14:04 -0500

    Import some used functions from base R

--- R/package.R
+++ R/package.R
@@ -5,10 +5,11 @@
 #' @importFrom Rcpp evalCpp
 #' @importFrom gplots heatmap.2
 #' @importFrom stats variable.names sd update heatmap runif
-#' @importFrom graphics matplot title
-#' @importFrom grDevices dev.new dev.off pdf
+#' @importFrom graphics matplot title abline close.screen hist legend lines mtext par plot points screen split.screen
+#' @importFrom grDevices dev.new dev.off pdf colorRampPalette rainbow
 #' @importFrom methods is
 #' @importFrom utils read.table write.table
 #' @importFrom RColorBrewer brewer.pal
+#' @importFrom stats cor loess
 #' @useDynLib CoGAPS
 NULL
