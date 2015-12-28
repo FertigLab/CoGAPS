2ae34f0cf8a5d855c4606a9a3b53caef7d1472f3
Jacob Carey <jacobcvt12@gmail.com>
2015-12-28 12:01:59 -0500

    Correct roxygen for package information.
    Now package.R populates CoGAPS-package.Rd, instead of CoGAPS.Rd, which
    was causing an error due to the same alias (CoGAPS-package) being in
    two different Rd files. The old CoGAPS.Rd file (mostly generated by
    Rcpp?) is overwritten by Roygen but the resulting file is mostly
    unchanged (a few formatting differences).

--- R/package.R
+++ R/package.R
@@ -1,7 +1,26 @@
-#' Coordinated Gene Activity in Pattern Sets
+#' CoGAPS: Coordinated Gene Activity in Pattern Sets
 #'
+#' CoGAPS implements a Bayesian MCMC matrix factorization algorithm,
+#' GAPS, and links it to gene set statistic methods to infer
+#' biological process activity.  It can be used to perform
+#' sparse matrix factorization on any data, and when this 
+#' data represents biomolecules, to do gene set analysis.
+#' \tabular{ll}{
+#' Package: \tab CoGAPS\cr
+#' Type: \tab Package\cr
+#' Version: \tab 1.0\cr
+#' Date: \tab 2014-07-23\cr
+#' License: \tab LGPL\cr
+#' } 
+#' @author Maintainer: Elana J. Fertig \email{ejfertig@jhmi.edu}, 
+#'             Michael F. Ochs \email{ochsm@tcnj.edu}
+#' @references 
+#' Fertig EJ, Ding J, Favorov AV, Parmigiani G, Ochs MF.  
+#' CoGAPS: an R/C++ package to identify patterns and biological
+#' process activity in transcriptomic data. 
+#' Bioinformatics. 2010 Nov 1;26(21):2792-3
 #' @docType package
-#' @name CoGAPS
+#' @name CoGAPS-package
 #' @importFrom Rcpp evalCpp
 #' @importFrom gplots heatmap.2 plotCI
 #' @importFrom stats variable.names sd update heatmap runif
