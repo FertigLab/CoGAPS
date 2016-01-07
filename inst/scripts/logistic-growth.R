188953979cddad833a4598ffe024bbc60a3d3b62
Jacob Carey <jacobcvt12@gmail.com>
2016-01-07 13:06:37 -0500

    Code for logistic growth pattern

--- inst/scripts/logistic-growth.R
+++ inst/scripts/logistic-growth.R
@@ -1,3 +1,5 @@
+library(ggplot)
+
 cogaps.utility <- function() {
     data(SimpSim)
     matplot(t(SimpSim.P))
@@ -6,6 +8,25 @@ cogaps.utility <- function() {
 }
 
 logistic.pattern <- function() {
+    data(SimpSim)
+    P <- SimpSim.P
+    A <- SimpSim.A
+    D.old <- SimpSim.D
+    S.old <- SimpSim.S
+
+    n <- 10
+    T <- seq(-5, 5, length.out = n)
+
+    p3.t <- logistic.growth(T, x.0=0, L=0.3, k=2)
+    p3.u <- logistic.growth(T, x.0=1, L=0.3, k=1)
+
+    data <- data.frame(t=c(T, T), y=c(p3.t, p3.u), status=rep(c("treated", "untreated"), each=n))
+
+    ggplot(data, aes(x=t, y=y)) +
+        geom_point(aes(colour=status)) +
+        geom_line(aes(colour=status))
+
+    P[3, ] <- c(p3.t, p3.u)
 }
 
 logistic.growth <- function(x, x.0=0.5, L=1, k=1) {
