a912ae6a427b1a2fd86c0f723836e158b9d78e32
Jacob Carey <jacobcvt12@gmail.com>
2016-01-06 23:22:24 -0500

    Move unused GibbsSampler methods to new cpp file
    This file will be delete in the next commit, for easy retrieval. The
    follow methods will be removed
    
    GibbsSampler (Takes filenames for data)
    output_computing_info
    cal_delloglikelihood_example
    computeDeltaLL2
    update_example
    atomicProposal2Matrix
    atomicProposal2FullMatrix
    get_AnnealingTemperature
    detail_check

--- src/GibbsSampler.cpp
+++ src/GibbsSampler.cpp
@@ -24,38 +24,6 @@ GibbsSampler:: GibbsSampler(unsigned long nEquil, unsigned long nSample, unsigne
                             double max_gibbsmass_paraA, double max_gibbsmass_paraP,
                             unsigned long long atomicSize,
                             char label_A, char label_P, char label_D, char label_S,
-                            const string &datafile, const string &variancefile,
-                            const string &simulation_id)
-    : _DMatrix(datafile.c_str(), label_D),
-      _SMatrix(variancefile.c_str(), label_S) {
-    _nEquil = nEquil;
-    _nSample = nSample;
-    _nFactor = nFactor;
-    _alphaA = alphaA;
-    _alphaP = alphaP;
-    _nMaxA = nMaxA;
-    _nMaxP = nMaxP;
-    _nIterA = nIterA;
-    _nIterP = nIterP;
-    _max_gibbsmass_paraA = max_gibbsmass_paraA;
-    _max_gibbsmass_paraP = max_gibbsmass_paraP;
-    _lambdaA_scale_factor = 1;
-    _lambdaP_scale_factor = 1;
-    _simulation_id = simulation_id;
-    _atomicSize = atomicSize;
-    _label_A = label_A;
-    _label_P = label_P;
-    _iter = 1;  // tmp use
-    _annealingTemperature = 0.4; // tmp use
-    _sysChi2 = 0.0; // tmp use
-}
-
-GibbsSampler:: GibbsSampler(unsigned long nEquil, unsigned long nSample, unsigned int nFactor,
-                            double alphaA, double alphaP, double nMaxA, double nMaxP,
-                            unsigned long nIterA, unsigned long nIterP,
-                            double max_gibbsmass_paraA, double max_gibbsmass_paraP,
-                            unsigned long long atomicSize,
-                            char label_A, char label_P, char label_D, char label_S,
                             vector<vector<double> > &DVector, vector<vector<double> > &SVector,
                             const string &simulation_id)
     : _DMatrix(DVector, label_D),
@@ -285,27 +253,6 @@ void GibbsSampler::output_atomicdomain(char atomic_label, unsigned long Samp_cyc
     }
 }
 
-// -----------------------------------------------------------------------------
-void GibbsSampler::output_computing_info(char outputFilename[],
-        unsigned long Equil_cycle, unsigned long nEquil,
-        unsigned long Samp_cycle, unsigned long nSample) {
-    ofstream outputFile;
-    outputFile.open(outputFilename, ios::out | ios::app);
-    outputFile << " *************************************************** " << endl;
-    outputFile << " --------------- NEW ROUND ------------------------- " << endl;
-    outputFile << " *************************************************** " << endl << endl;
-    outputFile << "Equilibration cycle index = " << Equil_cycle << endl;
-    outputFile << "Total number of equilibrating cycles to perform = " <<  nEquil << endl;
-    outputFile << "Sampling cycle index = " << Samp_cycle << endl;
-    outputFile << "Total number of sampling cycles to perform = " <<  nSample << endl;
-    outputFile << "System Chi2-value = " << _sysChi2 << endl;
-    _AMatrix.display_matrixF(outputFile);
-    _PMatrix.display_matrixF(outputFile);
-    check_resultsF(outputFile);
-    outputFile.close();
-}
-
-
 // ********* METHODS TO GO BETWEEN ATOMIC SPACE AND MATRIX  ********************
 unsigned int GibbsSampler::getRow(char matrix_label , unsigned int iBin) {
     switch (matrix_label) {
@@ -479,9 +426,6 @@ double GibbsSampler::cal_logLikelihood() {
     return GAPSNorm::calChi2(D, S, A, P, _nRow, _nCol, _nFactor) / 2.;
 }
 
-void GibbsSampler::cal_delloglikelihood_example() {
-}
-
 // -----------------------------------------------------------------------------
 double GibbsSampler::computeDeltaLL(char the_matrix_label,
                                     double const *const *D,
@@ -536,152 +480,6 @@ double GibbsSampler::computeDeltaLL(char the_matrix_label,
 
     return DelLL;
 } // end of computeDeltaLL
-
-
-// ---------------- For checking against computeDeltaLL
-double GibbsSampler::computeDeltaLL2(char the_matrix_label,
-                                     double const *const *D,
-                                     double const *const *S,
-                                     double const *const *A,
-                                     double const *const *P,
-                                     unsigned int the_nChange_matrixElemChange,
-                                     const vector<boost::tuple<unsigned int, unsigned int, double> > the_matrixElemChange) {
-    double DelLL = 0.0;
-
-    switch (the_matrix_label) {
-        case 'A': {
-            DelLL = GAPSNorm::calcDeltaLLGen('A', D, S, A, P, the_matrixElemChange, _nRow,
-                                             _nCol, _nFactor);
-            break;
-        }
-
-        case 'P': {
-            DelLL = GAPSNorm::calcDeltaLLGen('P', D, S, A, P, the_matrixElemChange, _nRow,
-                                             _nCol, _nFactor);
-            break;
-        }
-    }
-
-    return DelLL;
-} // end of computeDeltaLL2
-// -------------------------------------
-
-
-// *************** METHODS FOR MAKING PROPOSAL ********************************
-// -----------------------------------------------------------------------------
-void GibbsSampler::update_example(char atomic_domain_label) {
-} // end of update_example
-
-
-// -----------------------------------------------------------------------------
-vector<vector<double> > GibbsSampler::atomicProposal2Matrix(char atomic_domain_label,
-        double const *const *origMatrix) {
-    unsigned int bin;
-    unsigned int chRow, chCol;
-
-    switch (atomic_domain_label) {
-        case 'A': {
-            vector<vector<double> > newMatrix(_nRow, vector<double>(_nFactor, 0));
-            map<unsigned long long, double> proposal = _AAtomicdomain.getProposedAtoms();
-
-            for (map<unsigned long long, double>::const_iterator
-                    iter = proposal.begin(); iter != proposal.end(); ++ iter) {
-                bin = _AAtomicdomain.getBin(iter->first);
-                chRow = getRow('A', bin);
-                chCol = getCol('A', bin);
-                newMatrix[chRow][chCol] += iter->second;
-            }
-
-            return newMatrix;
-        } // end of case 'A'
-
-        case 'P': {
-            vector<vector<double> > newMatrix(_nFactor, vector<double>(_nCol, 0));
-            map<unsigned long long, double> proposal = _PAtomicdomain.getProposedAtoms();
-
-            for (map<unsigned long long, double>::const_iterator
-                    iter = proposal.begin(); iter != proposal.end(); ++ iter) {
-                bin = _PAtomicdomain.getBin(iter->first);
-                chRow = getRow('P', bin);
-                chCol = getCol('P', bin);
-                newMatrix[chRow][chCol] += iter->second;
-            }
-
-            return newMatrix;
-        } // end of case 'P'
-    } // end of switch
-
-    // EJF dummy return to avoid warning
-    vector<vector<double> > newMatrix(0, vector<double>(0, 0));
-    return newMatrix;
-} // end of atomicProposal2Matrix
-
-// -----------------------------------------------------------------------------
-vector<vector<double> > GibbsSampler::atomicProposal2FullMatrix(char atomic_domain_label,
-        double const *const *origMatrix) {
-    unsigned int bin;
-    unsigned int chRow, chCol;
-
-    switch (atomic_domain_label) {
-        case 'A': {
-            vector<vector<double> > FullnewMatrix(_nRow, vector<double>(_nFactor, 0));
-
-            for (unsigned int iRow = 0; iRow < _nRow; ++iRow) {
-                for (unsigned int iCol = 0; iCol < _nFactor; ++iCol) {
-                    if (origMatrix[iRow][iCol] < epsilon) {
-                        FullnewMatrix[iRow][iCol] = 0.;
-
-                    } else {
-                        FullnewMatrix[iRow][iCol] = origMatrix[iRow][iCol];
-                    }
-                }
-            } // end of for block
-
-            map<unsigned long long, double> proposal = _AAtomicdomain.getProposedAtoms();
-
-            for (map<unsigned long long, double>::const_iterator
-                    iter = proposal.begin(); iter != proposal.end(); ++ iter) {
-                bin = _AAtomicdomain.getBin(iter->first);
-                chRow = getRow('A', bin);
-                chCol = getCol('A', bin);
-                FullnewMatrix[chRow][chCol] += iter->second;
-            }
-
-            return FullnewMatrix;
-        } // end of case 'A'
-
-        case 'P': {
-            vector<vector<double> > FullnewMatrix(_nFactor, vector<double>(_nCol, 0));
-
-            for (unsigned int iRow = 0; iRow < _nFactor; ++iRow) {
-                for (unsigned int iCol = 0; iCol < _nCol; ++iCol) {
-                    if (origMatrix[iRow][iCol] < epsilon) {
-                        FullnewMatrix[iRow][iCol] = 0.;
-
-                    } else {
-                        FullnewMatrix[iRow][iCol] = origMatrix[iRow][iCol];
-                    }
-                }
-            } // end of for block
-
-            map<unsigned long long, double> proposal = _PAtomicdomain.getProposedAtoms();
-
-            for (map<unsigned long long, double>::const_iterator
-                    iter = proposal.begin(); iter != proposal.end(); ++ iter) {
-                bin = _PAtomicdomain.getBin(iter->first);
-                chRow = getRow('P', bin);
-                chCol = getCol('P', bin);
-                FullnewMatrix[chRow][chCol] += iter->second;
-            }
-
-            return FullnewMatrix;
-        } // end of case 'P'
-    } // end of switch
-
-    // EJF dummy return to avoid warning
-    vector<vector<double> > newMatrix(0, vector<double>(0, 0));
-    return newMatrix;
-} // end of atomicProposal2FullMatrix
 
 // ----------------------------------------------------------------------------
 void GibbsSampler::extract_atomicProposal(char the_matrix_label) {
@@ -1783,11 +1581,6 @@ void GibbsSampler::set_iter(unsigned long ext_iter) {
     _iter = ext_iter;
 }
 
-double GibbsSampler::get_AnnealingTemperature() {
-    return _annealingTemperature;
-}
-
-
 // -----------------------------------------------------------------------------
 void GibbsSampler::set_AnnealingTemperature() {
     double SASteps = _nEquil;
@@ -2267,13 +2060,4 @@ double GibbsSampler::getMass(char the_matrix_label, double origMass,
     }
 
     return newMass;
-}
-
-// -----------------------------------------------------------------------------
-void GibbsSampler::detail_check(char outputchi2_Filename[]) {
-    double chi2 = 2.*cal_logLikelihood();
-    ofstream outputchi2_File;
-    outputchi2_File.open(outputchi2_Filename, ios::out | ios::app);
-    outputchi2_File << chi2 << endl;
-    outputchi2_File.close();
 }
