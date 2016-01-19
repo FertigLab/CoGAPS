1b31968fd37100fe940a32e9b8d4511a2d76ddc0
Jacob Carey <jacobcvt12@gmail.com>
2016-01-19 13:06:33 -0500

    Close GibbsSamplerPattern class defn

--- src/GibbsSamplerPattern.h
+++ src/GibbsSamplerPattern.h
@@ -16,8 +16,9 @@ class GibbsSamplerPattern : public GibbsSamplerMap {
                         char label_A, char label_P, char label_D, char label_S,
                         vector<vector<double> > &DVector, vector<vector<double> > &SVector,
                         const string &simulation_id,
-                        vector <vector <double> >  &parameters, char the_fixed_matrix);
+                        vector <vector <double> >  &parameters, char the_fixed_matrix, int whichPattern);
 
     ~GibbsSamplerPattern() {};
+};
 
 #endif
