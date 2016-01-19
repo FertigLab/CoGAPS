b31b4f7a394be850aa38e35d215ff29a23012737
Jacob Carey <jacobcvt12@gmail.com>
2016-01-19 10:57:38 -0500

    Working JAGS script

--- inst/scripts/logistic-pattern.R
+++ inst/scripts/logistic-pattern.R
@@ -2,38 +2,38 @@ source("logistic-growth.R")
 
 library(R2jags)
 
-x.0 <- 0
+x.0 <- 2.5
 L <- 1
-k <- 2.5
+k <- 5
 N <- 1000
-#data.time <- seq(-3, 3, length.out = N)
-data.time <- runif(N, -3, 3)
-data.logit <- logistic.growth(data.time, x.0=x.0, L=L, k=k)
-qplot(data.time, data.logit)
-qplot(data.time, -log(L / data.logit - 1))
+data.time <- seq(-5, 5, length.out = N)
+# data.time <- runif(N, -3, 3)
+data.logit <- boot::logit(logistic.growth(data.time, x.0=x.0, L=L, k=k))
+# qplot(data.time, data.logit)
+# qplot(data.time, boot::logit(data.logit))
 
 pattern.model <- function() {
     # likelihood
     for (i in 1:N) {
-        transformation ~ dnorm(mean.norm, var.norm)
-        transformation <- log(L / data.logit[i] - 1)
+        data.logit[i] ~ dnorm(y.hat[i], tau)
+        y.hat[i] <- a + b * data.time[i]
     } 
 
-    # calculate mean and var
-    mean.norm <- k * x.0
-    var.norm <- 1000 * k ^ 2
-
-    # non-informative prior
-    k ~ dnorm(0, 1000)
+    # slope and intercept
+    a ~ dnorm(0, 0.1)
+    b ~ dnorm(0, 0.1)
+    tau <- pow(sigma, -2)
+    sigma ~ dunif(0, 1)
 }
 
-pattern.data <- c("L", "x.0", "data.logit", "N")
+pattern.data <- c("data.time", "data.logit", "N")
 
-pattern.params <- c("k")
+pattern.params <- c("a", "b")
 
 pattern.inits <- function() {
     inits <- list()
-    inits$k <- rnorm(1, mean=0, sd=10)
+    inits$a <- rnorm(1, mean=0, sd=10)
+    inits$b <- rnorm(1, mean=0, sd=10)
     return(inits)
 }
 
