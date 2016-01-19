65ff2a5ee571e2592af03977fd5c998005ce06e5
Jacob Carey <jacobcvt12@gmail.com>
2016-01-19 12:57:53 -0500

    Start working on new class
    For a priori pattern (i.e. logistic)

---
+++ src/GibbsSamplerPattern.cpp
@@ -0,0 +1,1 @@
+#include "GibbsSamplerMap.h"
---
+++ src/GibbsSamplerPattern.h
@@ -0,0 +1,23 @@
+#ifndef _GIBBSSAMPLERPATTER_H_
+#define _GIBBSSAMPLERPATTER_H_
+
+#include "GibbsSamplerMap.h"
+
+class GibbsSamplerPattern : public GibbsSamplerMap {
+  protected:
+    int _whichPattern;
+
+  public:
+    GibbsSamplerPattern(unsigned long nEquil, unsigned long nSample, unsigned int nFactor,
+                        double alphaA, double alphaP, double nMaxA, double nMaxP,
+                        unsigned long nIterA, unsigned long nIterP,
+                        double max_gibbsmass_paraA, double max_gibbsmass_paraP,
+                        unsigned long long atomicSize,
+                        char label_A, char label_P, char label_D, char label_S,
+                        vector<vector<double> > &DVector, vector<vector<double> > &SVector,
+                        const string &simulation_id,
+                        vector <vector <double> >  &parameters, char the_fixed_matrix);
+
+    ~GibbsSamplerPattern() {};
+
+#endif
