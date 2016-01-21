4aac7f63b5ef4aa5f028523ee4db15046463463b
Jacob Carey <jacobcvt12@gmail.com>
2016-01-21 14:05:44 -0500

    Add some notes from talk with Elana

--- inst/scripts/logistic-pattern.R
+++ inst/scripts/logistic-pattern.R
@@ -2,12 +2,25 @@ source("logistic-growth.R")
 
 library(R2jags)
 
+# use different language
+# update_transformation
+# add stupid checks for function
+
 x.0 <- 2.5
 L <- 1
 k <- 5
 N <- 100
 data.time <- seq(-5, 5, length.out = N)
 # data.time <- runif(N, -3, 3)
+# have to be careful with 0s and 1s
+# use machine epsilon
+# L can always be forced to 1
+# might be interested in x.0
+# always know the timepoints at which the timepoint was collected
+# how does this correspond to biological timepoint
+# have to be flexible with this
+# input variable
+# for now is fixed
 data.logit <- boot::logit(logistic.growth(data.time, x.0=x.0, L=L, k=k))
 # qplot(data.time, data.logit)
 # qplot(data.time, boot::logit(data.logit))
