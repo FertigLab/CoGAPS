746efdf482f66c5b30524d478e8ebac9ceb5ae68
Jacob Carey <jacobcvt12@gmail.com>
2016-02-03 12:43:28 -0500

    Add function for running Transfomraiton sampler

--- R/gapsTransRun.R
+++ R/gapsTransRun.R
@@ -1,6 +1,6 @@
-    results <- gapsRunTransformation(D, S, nFactor=3, 
-                                     # parameters for transformaiton routine
-                                     growth.trans="logistic", 
-                                     time.of.sample=1:ncol(D), # default
-                                     condition=matrix(1,  # treated/untreated (or treat1 treat2, etc, untreated)
-                                     nEquil=nBurn, nSample=nIter)
+ gapsRunTransformation <- function(D, S, nFactor=3, 
+                                   # parameters for transformaiton routine
+                                   growth.trans="logistic", 
+                                   time.of.sample=1:ncol(D), # default
+                                   condition=rep(0, ncol(D)),  # treated/untreated (or treat1 treat2, etc, untreated)
+                                   nEquil=nBurn, nSample=nIter)
