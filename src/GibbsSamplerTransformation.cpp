93e2f54471bea6678ef68e000e2dde87cdadcc0d
Jacob Carey <jacobcvt12@gmail.com>
2016-02-04 15:53:36 -0500

    Correct beta sampling

--- src/GibbsSamplerTransformation.cpp
+++ src/GibbsSamplerTransformation.cpp
@@ -68,12 +68,12 @@ void update_pattern(std::vector<double>(*transformation)(std::vector<double>)) {
         // \beta_0 | x, y, \beta_1, \tau
         post_var = 1. / (_tau0 + n * _tau);
         post_mean = (_tau0 * _mu0 + _tau * arma::sum(_y[i] - _beta1 * _x[i])) * post_var;
-        _beta0 = randgen("N", post_mean, sqrt(post_var));
+        _beta0[i] = randgen("N", post_mean, sqrt(post_var));
 
         // \beta_1 | x, y, \beta_0, \tau
         post_var = 1. / (_tau0 + _tau * arma::sum(pow(_x[i], 2.0)))
         post_mean = (_tau0 * _mu0 + _tau * arma::sum(_x[i] * (_y[i] - _beta1)) * post_var;
-        _beta1 = randgen("N", post_mean, sqrt(post_var));
+        _beta1[i] = randgen("N", post_mean, sqrt(post_var));
 
         // \tau | x, y, _beta0, _beta1
         post_shape = _a + n / 2.;
