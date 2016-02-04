60433512dd64d3d87564b73a93b2ecfd539c749c
Jacob Carey <jacobcvt12@gmail.com>
2016-02-04 14:36:34 -0500

    Change _timeRecorded to arma::vec

--- src/GibbsSamplerTransformation.h
+++ src/GibbsSamplerTransformation.h
@@ -14,7 +14,7 @@ class GibbsSamplerTransformation : public GibbsSamplerMap {
     // vector of case status (assume 0 is non-case) i.e. {0, 0, 0, 1, 1, 1}
     std::vector<int> _treatStatus;
     // vector of time recordings
-    std::vector<double> _timeRecorded;
+    arma::vec _timeRecorded;
 
     // priors for Gibbs Sampling of regression coefficients
     double _mu0 = 0.0;    // beta prior mean
