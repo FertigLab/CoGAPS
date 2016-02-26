b34441f84d09281da4d37d15fe2f3f8c2fcf55eb
Jacob Carey <jacobcvt12@gmail.com>
2016-02-26 12:58:45 -0500

    Experiment with less portion of logistic curve

--- inst/scripts/gibbs-trans.R
+++ inst/scripts/gibbs-trans.R
@@ -20,3 +20,20 @@ timeRecorded <- rep(T, 2)
 
 # cross fingers...
 testTrans <- test_run(y, treatStatus, timeRecorded, iter=2000)
+
+# now try with varying t_0 and t_f
+testing <- function(t0=-5, tf=5, n=100, rate.treat=2, rate.untreat=1) {
+    T <- seq(t0, tf, length.out = n)
+
+    p3.t <- logistic.growth(T, x.0=0, L=1, k=rate.treat) / 2
+    p3.u <- logistic.growth(T, x.0=1, L=1, k=rate.untreat) / 3
+
+    devtools::load_all("../trans")
+
+    y <- c(p3.t, p3.u)
+    treatStatus <- rep(0:1, each=n)
+    timeRecorded <- rep(T, 2)
+
+    # cross fingers...
+    test_run(y, treatStatus, timeRecorded, iter=2000)
+}
