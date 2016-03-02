4390054f82af0ea028968b65540691f893496ff9
Jacob Carey <jacobcvt12@gmail.com>
2016-03-02 12:17:40 -0500

    Normalize in logit transformation

--- src/transformations.h
+++ src/transformations.h
@@ -7,6 +7,9 @@
 // logistic transformation (log(p / 1 - p))
 Rcpp::NumericVector logit(Rcpp::NumericVector data) {
     Rcpp::NumericVector transformation(data.size());
+    double adj = Rcpp::max(data) + 1e-12;
+    Rcpp::NumericVector y;
+    y = transformation(y / (Rcpp::max(y) + 1e-12));
 
     for (int i = 0; i < data.size(); ++i) {
         transformation[i] = log(data[i] / (1 - data[i]));
