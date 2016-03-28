3c4dba18e0f15e92f20f2b5cdc39ba2ff2757090
Jacob Carey <jacobcvt12@gmail.com>
2016-03-28 09:07:29 -0400

    Compile

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -72,8 +72,8 @@ BEGIN_RCPP
 END_RCPP
 }
 // cogapsTrans
-Rcpp::List cogapsTrans(Rcpp::DataFrame DFrame, Rcpp::DataFrame SFrame, Rcpp::DataFrame FixedPatt, Rcpp::DataFrame ABinsFrame, Rcpp::DataFrame PBinsFrame, Rcpp::CharacterVector Config, Rcpp::NumericVector ConfigNums);
-RcppExport SEXP CoGAPS_cogapsTrans(SEXP DFrameSEXP, SEXP SFrameSEXP, SEXP FixedPattSEXP, SEXP ABinsFrameSEXP, SEXP PBinsFrameSEXP, SEXP ConfigSEXP, SEXP ConfigNumsSEXP) {
+Rcpp::List cogapsTrans(Rcpp::DataFrame DFrame, Rcpp::DataFrame SFrame, Rcpp::DataFrame FixedPatt, Rcpp::DataFrame ABinsFrame, Rcpp::DataFrame PBinsFrame, Rcpp::CharacterVector Config, Rcpp::NumericVector ConfigNums, std::vector<double> time_of_sample, std::vector<int> condition);
+RcppExport SEXP CoGAPS_cogapsTrans(SEXP DFrameSEXP, SEXP SFrameSEXP, SEXP FixedPattSEXP, SEXP ABinsFrameSEXP, SEXP PBinsFrameSEXP, SEXP ConfigSEXP, SEXP ConfigNumsSEXP, SEXP time_of_sampleSEXP, SEXP conditionSEXP) {
 BEGIN_RCPP
     Rcpp::RObject __result;
     Rcpp::RNGScope __rngScope;
@@ -84,7 +84,9 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< Rcpp::DataFrame >::type PBinsFrame(PBinsFrameSEXP);
     Rcpp::traits::input_parameter< Rcpp::CharacterVector >::type Config(ConfigSEXP);
     Rcpp::traits::input_parameter< Rcpp::NumericVector >::type ConfigNums(ConfigNumsSEXP);
-    __result = Rcpp::wrap(cogapsTrans(DFrame, SFrame, FixedPatt, ABinsFrame, PBinsFrame, Config, ConfigNums));
+    Rcpp::traits::input_parameter< std::vector<double> >::type time_of_sample(time_of_sampleSEXP);
+    Rcpp::traits::input_parameter< std::vector<int> >::type condition(conditionSEXP);
+    __result = Rcpp::wrap(cogapsTrans(DFrame, SFrame, FixedPatt, ABinsFrame, PBinsFrame, Config, ConfigNums, time_of_sample, condition));
     return __result;
 END_RCPP
 }
