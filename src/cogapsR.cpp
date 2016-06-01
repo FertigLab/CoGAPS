dd0452cce43e12af37428142a501f6b20a4edee7
Jacob Carey <jacobcvt12@gmail.com>
2016-05-31 22:39:01 -0400

    Move `rng` outside of function

--- src/cogapsR.cpp
+++ src/cogapsR.cpp
@@ -31,6 +31,8 @@ using namespace std;
 using namespace gaps;
 using std::vector;
 
+boost::mt19937 rng(43);
+
 // [[Rcpp::export]]
 Rcpp::List cogaps(Rcpp::DataFrame DFrame, Rcpp::DataFrame SFrame, Rcpp::DataFrame ABinsFrame,
                   Rcpp::DataFrame PBinsFrame, Rcpp::CharacterVector Config, Rcpp::NumericVector ConfigNums, int seed=-1) {
@@ -43,7 +45,6 @@ Rcpp::List cogaps(Rcpp::DataFrame DFrame, Rcpp::DataFrame SFrame, Rcpp::DataFram
     //boost::random::seed_seq seq(ve);
     //rng.seed(seq);
     // --- seeded with time
-    boost::mt19937 rng(43);
     if (seed <= 0) {
         rng.seed(static_cast<boost::uint32_t>(std::time(0)));
     } else {
