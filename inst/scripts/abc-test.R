b706e852573a584d068f9eb3296b35653c3cdcf8
Jacob Carey <JCarey@telligen.com>
2016-07-12 16:08:06 -0400

    Add epsilon parameter per Bortot et al 2007
    Has gamma prior and proposal

--- inst/scripts/abc-test.R
+++ inst/scripts/abc-test.R
@@ -43,17 +43,25 @@ A <- patts$A
 # one parameter, larger delta
 T <- seq(-5, 5, len=10)
 set.seed(20)
-iters <- 5000
-epsilon <- 3
+iters <- 40000
 
+# param: theta
 # prior: Gamma(1, 0.1)
 # proposal: Gamma(theta^2 / delta^2, theta / delta^2)
 prior.shape <- 1
 prior.rate <- 0.1
 delta <- 10
 
+# param: theta
+# prior: Gamma(1, 0.1)
+# proposal: Gamma(theta^2 / delta^2, theta / delta^2)
+shape <- 0.1
+rate <- 0.1
+delta.eps <- 10
+
 # intialize by sampling theta^{(0)} ~ pi(theta)
 theta <- rgamma(iters, prior.shape, prior.rate)
+epsilon <- rgamma(iters, shape, rate)
 
 for (i in 2:iters) {
     # message bar
@@ -61,7 +69,12 @@ for (i in 2:iters) {
   
     # simulate theta' ~ K(theta|theta^{(t-1)})
     theta.prime <- rgamma(1, theta[i-1] ^ 2 / delta^2, 
-                            theta[i-1] / delta^2)
+                          theta[i-1] / delta^2)
+    # simulate epsilon' ~ K(epsilon|epsilon^{(t-1)})
+    eps.prime <- rgamma(1, epsilon[i-1] ^ 2 / delta.eps^2, 
+                        epsilon[i-1] / delta.eps^2)
+    # don't allow epsilons less than 0.5
+    eps.prime <- max(eps.prime, 0.5)
 
     # simulate x ~ p(x | theta')
     growth <- logistic.growth(T, 0, 1, theta.prime)
@@ -73,21 +86,27 @@ for (i in 2:iters) {
     rho <- norm(diff, "2")
     
     # calculate acceptance probability
-    alpha <- dgamma(theta.prime, prior.shape, prior.rate, 
-                    log=TRUE) - 
-             dgamma(theta[i-1], prior.shape, prior.rate, 
-                    log=TRUE) +
+    alpha <- dgamma(theta.prime, prior.shape, prior.rate, log=TRUE) + 
+             dgamma(eps.prime, shape, rate, log=TRUE) + 
              dgamma(theta[i-1], theta.prime ^ 2 / delta ^2, 
-                    theta.prime / delta  ^ 2, log=TRUE) -
+                    theta.prime / delta  ^ 2, log=TRUE) +
+             dgamma(epsilon[i-1], eps.prime ^ 2 / delta.eps ^2, 
+                    eps.prime / delta.eps  ^ 2, log=TRUE) -
+             dgamma(theta[i-1], prior.shape, prior.rate, log=TRUE) - 
+             dgamma(epsilon[i-1], shape, rate, log=TRUE) - 
              dgamma(theta.prime, theta[i-1] ^ 2 / delta ^ 2, 
-                    theta[i-1] / delta ^ 2, log=TRUE)
-    
+                    theta[i-1] / delta  ^ 2, log=TRUE) -
+             dgamma(eps.prime, epsilon[i-1] ^ 2 / delta.eps ^2, 
+                    epsilon[i-1] / delta.eps  ^ 2, log=TRUE)
+      
     alpha <- exp(alpha)
-    alpha <- min(1, alpha * (rho < epsilon))
+    alpha <- min(1, alpha * (rho < eps.prime))
     
     # accept with probability alpha
     accept <- runif(1) < alpha
     theta[i] <- theta.prime * accept + theta[i-1] * (1-accept)
+    epsilon[i] <- eps.prime * accept + epsilon[i-1] * (1-accept)
 }
 
+ts.plot(epsilon)
 ts.plot(theta)
