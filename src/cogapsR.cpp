a3e80f3e671e14a3af7c33653ea1f628f65908e0
Jacob Carey <JCarey@telligen.com>
2016-07-28 16:35:32 -0400

    Convert A/P snapshots to 3d `arma::cube`
    Untested!

--- src/cogapsR.cpp
+++ src/cogapsR.cpp
@@ -23,7 +23,7 @@
 #include "GibbsSampler.h" // for incorporating the GibbsSampler which
 // does all the atomic space to matrix conversion
 // and sampling actions.
-#include <Rcpp.h>
+#include <RcppArmadillo.h>
 // ------------------------------------------------------
 
 //namespace bpo = boost::program_options;
@@ -498,36 +498,21 @@ Rcpp::List cogaps(Rcpp::DataFrame DFrame, Rcpp::DataFrame SFrame, Rcpp::DataFram
     int numSnaps = numSnapshots; //Arbitrary to keep convention
 
     if (SampleSnapshots == true) {
-        Rcpp::NumericVector ASnapR(Rcpp::Dimension(numSnaps, numRow, numCol));
-        //Rcpp::List ASnapR(numSnaps);
-        Rcpp::List PSnapR(numSnaps);
+        // http://stackoverflow.com/questions/30592738/how-to-convert-stl-vector-of-vector-to-armadillo-mat
         numRow = AMeanVector.size();
         numCol = AMeanVector[0].size() ;
-        Rcpp::NumericMatrix tempASnapMatrix(numRow, numCol);
+        arma::cube ASnapR(numRow, numCol, numSnaps));
 
         for (int k = 0; k < numSnaps; k++) {
-            for (int i = 0; i < numRow; i++) {
-                for (int j = 0; j < numCol; j++) {
-                    //tempASnapMatrix(i, j) = ASnap[k][i][j] ;
-                    //ASnapR(k, i, j) = ASnap[k][i][j];
-                }
-            }
-
-            //ASnapR.push_back(tempASnapMatrix);
+            ASnapR.slice(k) = arma::conv_to<arma::mat>::from(ASnap[k]);
         }
 
         numRow = PMeanVector.size();
         numCol = PMeanVector[0].size() ;
-        Rcpp::NumericMatrix tempPSnapMatrix(numRow, numCol);
+        arma::cube PSnapR(numRow, numCol, numSnaps);
 
         for (int k = 0; k < numSnaps; k++) {
-            for (int i = 0; i < numRow; i++) {
-                for (int j = 0; j < numCol; j++) {
-                    tempPSnapMatrix(i, j) = PSnap[k][i][j] ;
-                }
-            }
-
-            PSnapR[k] = (tempPSnapMatrix);
+            PSnapR.slice(k) = arma::conv_to<arma::mat>::from(PSnap[k]);
         }
 
         Rcpp::List fileContainer =  Rcpp::List::create(Rcpp::Named("Amean") = AMeanMatrix,
