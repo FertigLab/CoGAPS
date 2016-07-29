84c128c2e5be86e988350f845f69fad0eaadf210
Jacob Carey <JCarey@telligen.com>
2016-07-29 12:23:42 -0400

    Add gapsMap test

--- tests/testthat/test_gaps.R
+++ tests/testthat/test_gaps.R
@@ -6,3 +6,11 @@ test_that("GAPS Simple Simulation", {
     results <- gapsRun(SimpSim.D, SimpSim.S, nFactor=3,
             nEquil=nIter, nSample=nIter)
 })
+
+test_that("GAPSmap Simple Simulation", {
+    data(SimpSim)
+    FP <- matrix(SimpSim.P[3, ], nrow=1)
+    nIter <- 5000
+    results <- gapsMapRun(SimpSim.D, SimpSim.S, FP, nFactor=3,
+                          nEquil=nIter, nSample=nIter)
+})
