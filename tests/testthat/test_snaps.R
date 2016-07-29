cc94f68d3679b450ee3d444a3e2af69e5d57e43b
Jacob Carey <JCarey@telligen.com>
2016-07-29 13:13:38 -0400

    Use messages in unit tests

--- tests/testthat/test_snaps.R
+++ tests/testthat/test_snaps.R
@@ -10,7 +10,7 @@ test_that("Distinct gaps Snapshots", {
 test_that("Distinct gapsMap Snapshots", {
     data(SimpSim)
     FP <- matrix(SimpSim.P[3, ], nrow=1)
-    x <- gapsMapRun(SimpSim.D, SimpSim.S, FP, nFactor=3)
+    x <- gapsMapRun(SimpSim.D, SimpSim.S, FP, nFactor=3, messages=FALSE)
     expect_false(identical(x$ASnapshots[, , 1], x$ASnapshots[, , 100]))
     expect_false(identical(x$PSnapshots[, , 1], x$PSnapshots[, , 100]))
 })
