42e741f2c308ae5345e550faee5ae39388b72643
Jacob Carey <JCarey@telligen.com>
2016-07-29 12:24:36 -0400

    Decrease iters in GAPS context

--- tests/testthat/test_gaps.R
+++ tests/testthat/test_gaps.R
@@ -2,7 +2,7 @@ context("GAPS")
 
 test_that("GAPS Simple Simulation", {
     data(SimpSim)
-    nIter <- 5000
+    nIter <- 1000
     results <- gapsRun(SimpSim.D, SimpSim.S, nFactor=3,
             nEquil=nIter, nSample=nIter)
 })
@@ -10,7 +10,7 @@ test_that("GAPS Simple Simulation", {
 test_that("GAPSmap Simple Simulation", {
     data(SimpSim)
     FP <- matrix(SimpSim.P[3, ], nrow=1)
-    nIter <- 5000
+    nIter <- 1000
     results <- gapsMapRun(SimpSim.D, SimpSim.S, FP, nFactor=3,
                           nEquil=nIter, nSample=nIter)
 })
