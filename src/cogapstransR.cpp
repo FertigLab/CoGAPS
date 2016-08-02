3a251f9f54c41d8a096a102b41d204b6b6042458
Jacob Carey <jacobcvt12@gmail.com>
2016-08-02 08:30:58 -0400

    Pass args from `cogapstrans` to `GST` constructor

--- src/cogapstransR.cpp
+++ src/cogapstransR.cpp
@@ -43,7 +43,9 @@ Rcpp::List cogapsTrans(Rcpp::DataFrame DFrame,
                        Rcpp::NumericVector ConfigNums,
                        std::vector<double> time_of_sample,
                        std::vector<int> condition,
-                       double tolerance) {
+                       std::string prior="normal", std::string proposal="normal",
+                       bool epsilon_mcmc=false, double delta=10.0, double epsilon=100.0,
+                       double epsilon_prior=3.0, double prior_mean=0.0, double prior_sd=10.0) {
     // ===========================================================================
     // Initialization of the random number generator.
     // Different seeding methods:
@@ -203,7 +205,11 @@ Rcpp::List cogapsTrans(Rcpp::DataFrame DFrame,
                                               atomicSize,
                                               label_A, label_P, label_D, label_S,
                                               DVector, SVector, simulation_id, FPVector, label_FP, 3,
-                                              condition, time_of_sample, tolerance);
+                                              condition, time_of_sample,
+                                              std::string prior, std::string proposal,
+                                              bool epsilon_mcmc, double delta, 
+                                              double epsilon, double epsilon_prior, 
+                                              double prior_mean, double prior_sd);
     int thin = 5;
     // ---------------------------------------------------------------------------
     // Based on the information of D, construct and initialize for A and P both
