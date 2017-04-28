3bc5be90e2c662c515df0ad548d3b9a1da34e0aa
Jacob Carey <jacobcvt12@gmail.com>
2017-04-28 11:04:27 -0400

    Apply weighting to atoms in A col

--- src/cogapstransR.cpp
+++ src/cogapstransR.cpp
@@ -368,6 +368,7 @@ Rcpp::List cogapsTrans(Rcpp::DataFrame DFrame,
 
         // update pattern
         GibbsSampTrans.abc_mcmc(ext_iter, 0, thin);
+        GibbsSampTrans.weightAAtomicColumn(GibbsSampTrans.new_weight / GibbsSampTrans.old_weight);
 
         Rcpp::Rcout << "finish theta\n";
 
