397311ece63a98a15bdce551e56ce684d8437dc2
sherman5 <tomsherman159@gmail.com>
2017-09-22 15:42:54 -0400

    repaired cpp unit test framework

---
+++ src/test-runner.cpp
@@ -0,0 +1,10 @@
+#define CATCH_CONFIG_RUNNER
+#include "cpp_tests/catch.h"
+
+// [[Rcpp::export]]
+int run_catch_unit_tests()
+{
+    Catch::Session session;
+    int numFailed = session.run();
+    return (numFailed < 0xFF ? numFailed : 0xFF);
+}
