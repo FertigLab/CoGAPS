30d20d1afdf2fb4a38688173cb254b31a1850db4
sherman5 <tomsherman159@gmail.com>
2017-11-29 16:58:26 -0500

    added basic benchmark structure

--- R/gapsRun.R
+++ R/gapsRun.R
@@ -228,7 +228,8 @@ gapsRun <- function(D, S, ABins = data.frame(), PBins = data.frame(),
 
   names(cogapResult)[13] = "meanChi2";
 
-  message(paste("Chi-Squared of Mean:",calcChiSq))
+  if (messages)
+    message(paste("Chi-Squared of Mean:",calcChiSq))
 
   return(cogapResult);
 }
