2724569b443f21ea233a67e6413d357476383f22
sherman5 <tomsherman159@gmail.com>
2017-11-29 20:20:54 -0500

    sub_func implementations

--- src/Random.h
+++ src/Random.h
@@ -16,14 +16,14 @@ namespace Random
     double normal(double mean, double var);
     double exponential(double lambda);
 
-    double pexp(double p, double rate, bool boolpara1, bool boolpara2);
-    double qexp(double q, double rate, bool boolpara1, bool boolpara2);
-    double dgamma(double newMass, double shape, double scale, bool boolpara);
-    double pgamma(double p, double shape, double scale, bool boolpara1, bool boolpara2);
-    double qgamma(double q, double shape, double scale, bool boolpara1, bool boolpara2);
-    double dnorm(double u, double mean, double sd, bool unknown);
-    double qnorm(double u, double mean, double sd, double INF_Ref, double unknown);
-    double pnorm(double u, double mean, double sd, double INF_Ref, double unknown);
+    double pexp(double p, double rate);
+    double qexp(double q, double rate);
+    double dgamma(double d, double shape, double scale);
+    double pgamma(double p, double shape, double scale);
+    double qgamma(double q, double shape, double scale);
+    double dnorm(double d, double mean, double sd);
+    double qnorm(double q, double mean, double sd);
+    double pnorm(double p, double mean, double sd);
 }
 
 #endif
