c9477d854a4a39356c4c211cc0087bd650ac4895
sherman5 <tomsherman159@gmail.com>
2017-12-03 20:35:31 -0500

    added profiling

---
+++ inst/profiling/profile_standard.R
@@ -0,0 +1,10 @@
+library(CoGAPS)
+
+data(SimpSim)
+nIter <- 1000
+gapsRun(SimpSim.D, SimpSim.S, nFactor=3, messages=FALSE, seed=12345)
+
+#data(GIST_TS_20084)
+#nIter <- 3000
+#gapsRun(D=GIST.D[1:75,], S=GIST.S[1:75,], nEquil=nIter, nSample=nIter,
+#    nFactor=7, seed=12345)
