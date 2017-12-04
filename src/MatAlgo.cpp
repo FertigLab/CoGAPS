2f43feeec389aaaedd0de548dd5ff7fa871ba7ca
sherman5 <tomsherman159@gmail.com>
2017-12-03 20:18:54 -0500

    added separate namespace for doing matrix computations

---
+++ src/MatAlgo.cpp
@@ -0,0 +1,83 @@
+#include "MatAlgo.h"
+#include "Matrix.h"
+
+#include <algorithm>
+
+matrix_data_t MatAlgo::mean(const RowMatrix &mat)
+{
+    return MatAlgo::sum(mat) / (mat.nRow() * mat.nCol());
+}
+
+matrix_data_t MatAlgo::sum(const RowMatrix &mat)
+{
+    matrix_data_t sum = 0.0;
+    for (unsigned r = 0; r < mat.nRow(); ++r)
+    {
+        sum += mat.getRow(r).sum();
+    }
+    return sum;
+}
+
+void MatAlgo::matrixMultiplication(Matrix &result, const Matrix &A, const Matrix &B)
+{
+    if (result.nRow() != A.nRow() ||
+        result.nCol() != B.nCol() ||
+        A.nCol() != B.nRow())
+    {
+        throw std::invalid_argument("invalid matrix dimensions");
+    }
+
+    for (unsigned int r = 0; r < result.nRow(); ++r)
+    {
+        for (unsigned int c = 0; c < result.nCol(); ++c)
+        {
+            result(r,c) = 0;
+            for (unsigned int k = 0; k < A.nCol(); ++k)
+            {
+                result(r,c) += A(r,k) * B(k,c);
+            }
+        }
+    }
+}
+
+void MatAlgo::matrixSubtraction(Matrix &result, const Matrix &A, const Matrix &B)
+{
+    if (result.nRow() != A.nRow() ||
+        result.nRow() != B.nRow() ||
+        result.nCol() != A.nCol() ||
+        result.nCol() != B.nCol() ||
+        A.nRow() != B.nRow() ||
+        A.nCol() != B.nCol())
+    {
+        throw std::invalid_argument("invalid matrix dimensions");
+    }
+
+    for (unsigned int r = 0; r < result.nRow(); ++r)
+    {
+        for (unsigned int c = 0; c < result.nCol(); ++c)
+        {
+            result(r,c) = A(r,c) - B(r,c);
+        }
+    }
+}
+
+void MatAlgo::elementWiseMatrixMultiplication(Matrix &result, const Matrix &A, const Matrix &B)
+{
+    if (result.nRow() != A.nRow() ||
+        result.nRow() != B.nRow() ||
+        result.nCol() != A.nCol() ||
+        result.nCol() != B.nCol() ||
+        A.nRow() != B.nRow() ||
+        A.nCol() != B.nCol())
+    {
+        throw std::invalid_argument("invalid matrix dimensions");
+    }
+
+    for (unsigned int r = 0; r < result.nRow(); ++r)
+    {
+        for (unsigned int c = 0; c < result.nCol(); ++c)
+        {
+            result(r,c) = A(r,c) * B(r,c);
+        }
+    }
+}
---
+++ src/MatAlgo.h
@@ -0,0 +1,15 @@
+#ifndef __COGAPS_MATRIX_ALGORITHM_H__
+#define __COGAPS_MATRIX_ALGORITHM_H__
+
+#include "Matrix.h"
+
+namespace MatAlgo
+{
+    matrix_data_t sum(const Matrix &mat);
+    matrix_data_t mean(const Matrix &mat);
+    void matrixMultiplication(Matrix &result, const Matrix &A, const Matrix &B);
+    void matrixSubtraction(Matrix &result, const Matrix &A, const Matrix &B);
+    void elementWiseMatrixMultiplication(Matrix &result, const Matrix &A, const Matrix &B);
+}
+
+#endif
