7ec69a138a44a46524dc9098d797ebe1cf3e4148
sherman5 <tomsherman159@gmail.com>
2017-12-11 22:25:07 -0500

    added benchmark

--- R/gapsRun.R
+++ R/gapsRun.R
@@ -197,41 +197,41 @@ gapsRun <- function(D, S, ABins = data.frame(), PBins = data.frame(),
     singleCellRNASeq);
 
   # convert returned files to matrices to simplify visualization and processing
-  cogapResult$Amean = as.matrix(cogapResult$Amean);
-  cogapResult$Asd = as.matrix(cogapResult$Asd);
-  cogapResult$Pmean = as.matrix(cogapResult$Pmean);
-  cogapResult$Psd = as.matrix(cogapResult$Psd);
-
-  # label matrices
-  colnames(cogapResult$Amean) = patternNames;
-  rownames(cogapResult$Amean) = geneNames;
-  colnames(cogapResult$Asd) = patternNames;
-  rownames(cogapResult$Asd) = geneNames;
-  colnames(cogapResult$Pmean) = sampleNames;
-  rownames(cogapResult$Pmean) = patternNames;
-  colnames(cogapResult$Psd) = sampleNames;
-  rownames(cogapResult$Psd) = patternNames;
-
-  # calculate chi-squared of mean, this should be smaller than individual
-  # chi-squared sample values if sampling is good
-  calcChiSq = c(0);
-  MMatrix = (cogapResult$Amean %*% cogapResult$Pmean);
-
-
-  for(i in 1:(nrow(MMatrix)))
-  {
-    for(j in 1:(ncol(MMatrix)))
-    {
-      calcChiSq = (calcChiSq) + ((D[i,j] - MMatrix[i,j])/(S[i,j]))^(2);
-    }
-  }
-
-  cogapResult = c(cogapResult, calcChiSq);
-
-  names(cogapResult)[13] = "meanChi2";
-
-  if (messages)
-    message(paste("Chi-Squared of Mean:",calcChiSq))
+  #cogapResult$Amean = as.matrix(cogapResult$Amean);
+  #cogapResult$Asd = as.matrix(cogapResult$Asd);
+  #cogapResult$Pmean = as.matrix(cogapResult$Pmean);
+  #cogapResult$Psd = as.matrix(cogapResult$Psd);
+#
+  ## label matrices
+  #colnames(cogapResult$Amean) = patternNames;
+  #rownames(cogapResult$Amean) = geneNames;
+  #colnames(cogapResult$Asd) = patternNames;
+  #rownames(cogapResult$Asd) = geneNames;
+  #colnames(cogapResult$Pmean) = sampleNames;
+  #rownames(cogapResult$Pmean) = patternNames;
+  #colnames(cogapResult$Psd) = sampleNames;
+  #rownames(cogapResult$Psd) = patternNames;
+#
+  ## calculate chi-squared of mean, this should be smaller than individual
+  ## chi-squared sample values if sampling is good
+  #calcChiSq = c(0);
+  #MMatrix = (cogapResult$Amean %*% cogapResult$Pmean);
+#
+#
+  #for(i in 1:(nrow(MMatrix)))
+  #{
+  #  for(j in 1:(ncol(MMatrix)))
+  #  {
+  #    calcChiSq = (calcChiSq) + ((D[i,j] - MMatrix[i,j])/(S[i,j]))^(2);
+  #  }
+  #}
+#
+  #cogapResult = c(cogapResult, calcChiSq);
+#
+  #names(cogapResult)[13] = "meanChi2";
+#
+  #if (messages)
+  #  message(paste("Chi-Squared of Mean:",calcChiSq))
 
   return(cogapResult);
 }
---
+++ inst/benchmarks/topLevelBenchmark.R
@@ -0,0 +1,101 @@
+# save file time at beginning to ensure uniqueness
+file_name <- paste("cogaps_benchmark_", format(Sys.time(), "%m_%d_%y_%H_%M_%OS"),
+    '.RData', sep='')
+
+# load packages
+library(CoGAPS)
+
+# display package version
+print(packageVersion('CoGAPS'))
+
+# benchmark dimensions
+
+#M_dimensions <- c(10, 25, 50, 100, 250, 500, 750, 1000, 2500, 5000)
+#N_dimensions <- c(10, 25, 50, 100, 250, 500, 750, 1000, 2500, 5000)
+#nFactor_dimensions <- c(3, 5, 7, 10, 15, 20, 25, 30, 40, 50, 75, 100)
+#nIter_dimensions <- c(1000, 1250, 1500, 1750, 2000, 2500, 3000, 4000, 5000)
+
+M_dimensions <- c(10, 25, 50)
+N_dimensions <- c(10, 25, 50)
+nFactor_dimensions <- c(3, 5, 7)
+nIter_dimensions <- c(1000, 1250)
+
+M_default <- 100
+N_default <- 100
+nFactor_default <- 7
+nIter_default <- 1000
+
+seed_default <- 12345
+reps_default <- 5
+
+# benchmark function
+
+data(GIST_TS_20084)
+runBenchmark <- function(m, n, k, iter, seed, reps)
+{
+    set.seed(123)
+    test_mat_D <- matrix(sample(as.matrix(GIST.D), m * n, replace=T),
+        nrow = m, ncol = n)
+
+    test_mat_S <- matrix(sample(as.matrix(GIST.S), m * n, replace=T),
+        nrow = m, ncol = n)
+
+    times <- c()
+    for (r in 1:reps)
+    {
+        start_time <- Sys.time()
+        gapsRun(test_mat_D, test_mat_S, nEquil=iter, nSample=iter,
+            nFactor=k, seed=seed+r, messages=FALSE)
+        times[r] <- as.numeric(Sys.time() - start_time)
+    }
+    params <- c(m, n, k, iter, seed, reps)
+    secs <- c(min(times), mean(times), median(times), max(times))
+    return(c(params, secs))
+}
+
+# store sample times and parameters
+samples <- NULL
+
+# run M benchmark
+for (n in M_dimensions)
+{
+    print(paste("benchmarking M =", n))
+    bm <- runBenchmark(n, N_default, nFactor_default, nIter_default, seed_default,
+        reps_default)
+    samples <- rbind(samples, bm)
+}
+
+# run N benchmark
+for (n in N_dimensions)
+{
+    print(paste("benchmarking N =", n))
+    bm <- runBenchmark(M_default, n, nFactor_default, nIter_default, seed_default,
+        reps_default)
+    samples <- rbind(samples, bm)
+}
+
+# run nFactor benchmark
+for (n in nFactor_dimensions)
+{
+    print(paste("benchmarking K =", n))
+    bm <- runBenchmark(M_default, N_default, n, nIter_default, seed_default,
+        reps_default)
+    samples <- rbind(samples, bm)
+}
+
+# run nIter benchmark
+for (n in nIter_dimensions)
+{
+    print(paste("benchmarking nIter =", n))
+    bm <- runBenchmark(M_default, N_default, nFactor_default, n, seed_default,
+        reps_default)
+    samples <- rbind(samples, bm)
+}
+
+rownames(samples) <- NULL
+colnames(samples) <- c('M', 'N', 'K', 'nIter', 'seed', 'reps', 'min', 'mean',
+    'median', 'max')
+cogaps_benchmark <- data.frame(samples)
+cogaps_version <- packageVersion('CoGAPS')
+
+save(cogaps_benchmark, cogaps_version, file=file_name)
