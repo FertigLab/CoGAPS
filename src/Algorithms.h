8048f6b712868a1578f8bc0701e330d96b19d5b7
sherman5 <tomsherman159@gmail.com>
2018-01-05 11:26:43 -0500

    comment out new class for testing

--- src/Algorithms.h
+++ src/Algorithms.h
@@ -21,7 +21,7 @@ struct AlphaParameters
     }
 };
 
-class vec4f
+/*class vec4f
 {
 private:
     
@@ -44,12 +44,13 @@ public:
 
     inline operator __m128() const {return mValue;}
 
-    /*inline vec4f& operator+=(const vec4f &rhs)
+    inline vec4f& operator+=(const vec4f &rhs)
     {
         *this = *this + rhs;
         return *this;
-    }*/
+    }
 };
+*/
 
 /*inline vec4f operator+(const vec4f &lhs, const vec4f &rhs)
 {
