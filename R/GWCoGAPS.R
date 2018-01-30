d9e7e41cb77144e67c9f755fb30a330602eb7249
sherman5 <tomsherman159@gmail.com>
2018-01-30 15:50:37 -0500

    parallel checkpoints mostly working - some bugs with processing output

--- R/GWCoGAPS.R
+++ R/GWCoGAPS.R
@@ -3,12 +3,10 @@
 #' @details calls the C++ MCMC code and performs Bayesian
 #' matrix factorization returning the two matrices that reconstruct
 #' the data matrix for whole genome data;
-#' @param D data matrix
-#' @param S uncertainty matrix (std devs for chi-squared of Log Likelihood)
 #' @param nFactor number of patterns (basis vectors, metagenes), which must be
 #' greater than or equal to the number of rows of FP
 #' @param nCores number of cores for parallelization. If left to the default NA, nCores = nSets.
-#' @param Cut number of branches at which to cut dendrogram used in patternMatch4Parallel
+#' @param cut number of branches at which to cut dendrogram used in patternMatch4Parallel
 #' @param minNS minimum of individual set contributions a cluster must contain
 #' @param ... additional parameters to be fed into \code{gapsRun} and \code{gapsMapRun}
 #' @return list of A and P estimates
@@ -21,21 +19,23 @@
 #' @export
 GWCoGAPS <- function(simulationName, nFactor, nCores=NA, cut=NA, minNS=NA, ...)
 {
+    if (!is.null(list(...)$checkpointFile))
+        stop("checkpoint file name automatically set in GWCoGAPS - don't pass this parameter")
     allDataSets <- preInitialPhase(simulationName, nCores)
-    initialResult <- runInitialPhase(allDataSets, nFactor)
-    matchedPatternSets <- postInitialPhase(initialResult, length(allDataSets), cut)
-    finalResult <- runFinalPhase(allDataSets, matchedPatternSets)
-    return(postFinalPhase(allDataSets, matchedPatternSets))
+    initialResult <- runInitialPhase(simulationName, allDataSets, nFactor, ...)
+    matchedPatternSets <- postInitialPhase(initialResult, length(allDataSets), cut, minNS)
+    finalResult <- runFinalPhase(simulationName, allDataSets, matchedPatternSets, ...)
+    return(postFinalPhase(finalResult, matchedPatternSets))
 }
 
-GWCoGapsFromCheckpoint <- function(simulationName, nCores=NA, cut=NA)
+GWCoGapsFromCheckpoint <- function(simulationName, nCores=NA, cut=NA, minNS=NA, ...)
 {
     allDataSets <- preInitialPhase(simulationName, nCores)
 
     # figure out phase from file signature
-    initalCpts <- list.files(full.names=TRUE, pattern=paste(simulationName, "_initial_run_*.out"))
-    finalCpts <- list.files(full.names=TRUE, pattern=paste(simulationName, "_final_run_*.out"))
-    cptPrefix <- ifelse(length(finalCpts), "_final_run_", "_initial_run_")
+    initalCpts <- list.files(full.names=TRUE, pattern=paste(simulationName, "_initial_cpt_[0-9]+.out"))
+    finalCpts <- list.files(full.names=TRUE, pattern=paste(simulationName, "_final_cpt_[0-9]+.out"))
+    cptPrefix <- ifelse(length(finalCpts), "_final_cpt_", "_initial_cpt_")
 
     # run CoGAPS for each set, starting from appropiate checkpoint file
     result <- foreach(i=1:nSets) %dopar%
@@ -55,8 +55,8 @@ GWCoGapsFromCheckpoint <- function(simulationName, nCores=NA, cut=NA)
     }
     else 
     {
-        matchedPatternSets <- postInitialPhase(result, length(allDataSets), cut)
-        finalResult <- runFinalPhase(allDataSets, matchedPatternSets)
+        matchedPatternSets <- postInitialPhase(result, length(allDataSets), cut, minNS)
+        finalResult <- runFinalPhase(allDataSets, matchedPatternSets, ...)
         return(postFinalPhase(allDataSets, matchedPatternSets))
     }
 }
@@ -64,8 +64,8 @@ GWCoGapsFromCheckpoint <- function(simulationName, nCores=NA, cut=NA)
 preInitialPhase <- function(simulationName, nCores)
 {
     # find data files
-    fileSig <- paste(simulationName, "_partition_*.RData", sep="")
-    allDataSets <- list.files(full.names=TRUE, pattern = fileSig)
+    fileSig <- paste(simulationName, "_partition_[0-9]+.RData", sep="")
+    allDataSets <- list.files(full.names=TRUE, pattern=fileSig)
 
     # establish the number of cores that we are able to use
     if (is.na(nCores))
@@ -74,7 +74,7 @@ preInitialPhase <- function(simulationName, nCores)
     return(allDataSets)
 }
 
-runInitialPhase <- function(allDataSets, nFactor)
+runInitialPhase <- function(simulationName, allDataSets, nFactor, ...)
 {
     #generate seeds for parallelization
     nut <- generateSeeds(chains=length(allDataSets), seed=-1)
@@ -87,14 +87,14 @@ runInitialPhase <- function(allDataSets, nFactor)
         sampleD <- sweep(sampleD, 1, apply(sampleD, 1, function(x) pmin(0,min(x))))
 
         # run CoGAPS without any fixed patterns
-        cptFileName <- paste(simulationName, "_initial_run_", i, ".out", sep="")
+        cptFileName <- paste(simulationName, "_initial_cpt_", i, ".out", sep="")
         CoGAPS(sampleD, sampleS, nFactor=nFactor, seed=nut[i],
             checkpointFile=cptFileName, ...)
     }
     return(initialResult)
 }
 
-postInitialPhase <- function(initialResult, nSets, cut)
+postInitialPhase <- function(initialResult, nSets, cut, minNS)
 {
     nFactor <- ncol(initialResult[[1]]$Amean)
     BySet <- reOrderBySet(AP=initialResult, nFactor=nFactor, nSets=nSets)
@@ -106,32 +106,34 @@ postInitialPhase <- function(initialResult, nSets, cut)
         minNS=minNS, bySet=TRUE))
 }
 
-runFinalPhase <- function(allDataSets, matchedPatternSets)
+runFinalPhase <- function(simulationName, allDataSets, matchedPatternSets, ...)
 {
     # generate seeds for parallelization
     nut <- generateSeeds(chains=length(allDataSets), seed=-1)
 
     # final number of factors
-    nFactorFinal <- dim(matchedPatternSets)[1]
+    nFactorFinal <- nrow(matchedPatternSets[[1]])
 
     # run fixed CoGAPS
-    finalResult <- foreach(i=1:length(allDataSets) %dopar%
+    finalResult <- foreach(i=1:length(allDataSets)) %dopar%
     {
         # load data set and shift values so gene minimum is zero
         load(allDataSets[[i]])
         sampleD <- sweep(sampleD, 1, apply(sampleD, 1, function(x) pmin(0,min(x))))
 
         # run CoGAPS with fixed patterns
-        cptFileName <- paste(simulationName, "_final_run_", i, ".out", sep="")
-        CoGAPS(sampleD, sampleS, FP=matchedPs, nFactor=nFactorFinal, seed=nut[i],
-            checkpointFile=cptFileName, ...)
+        cptFileName <- paste(simulationName, "_final_cpt_", i, ".out", sep="")
+        CoGAPS(sampleD, sampleS, fixedPatterns=matchedPatternSets[[1]],
+            nFactor=nFactorFinal, seed=nut[i], checkpointFile=cptFileName,
+            whichMatrixFixed='P', ...)
+
     }
     return(finalResult)
 }
 
 postFinalPhase <- function(finalResult, matchedPatternSets)
 {
-    Aresult <- postFixed4Parallel(finalResult, matchedPatternSets)
+    Aresult <- postFixed4Parallel(finalResult, matchedPatternSets[[1]])
     finalResult <- list("Amean"=Aresult$A, "Asd"=Aresult$Asd,
         "Pmean"=matchedPatternSets, "PbySet"=matchedPatternSets[["PBySet"]])
     class(finalResult) <- append(class(finalResult), "CoGAPS")
