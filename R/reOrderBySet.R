47ef0da9f22e0d7c1c063ab41ece9f4e0e21eb93
sherman5 <tomsherman159@gmail.com>
2018-02-04 20:45:26 -0500

    formatting

--- R/reOrderBySet.R
+++ R/reOrderBySet.R
@@ -6,18 +6,10 @@
 #' @param nSets number of sets
 #' @param match which matrix to use for downstream matching. default is P
 #' @return a list containing the \code{nSets} sets solution for Amean under "A", Pmean under "P", and Asd under "Asd"
-#' @export
-#'
-#' @examples \dontrun{
-#' reOrderBySet(AP,nFactor,nSets)
-#' }
-#'
-reOrderBySet<-function(AP, #output of gapsRun in parallel
-	nFactor, #number of patterns
-	nSets, #number of sets for parallelization
-	match="P"
-){
-	if(match=="P"){
+reOrderBySet<-function(AP, nFactor, nSets, match="P")
+{
+	if(match=="P")
+	{
 		P<-do.call(rbind,lapply(AP, function(x) x$Pmean))
 		rownames(P)<-paste(rep(1:nSets,each=nFactor),rep(1:nFactor,nSets),sep=".")
 		A<-lapply(AP, function(x) x$Amean)
@@ -25,7 +17,9 @@ reOrderBySet<-function(AP, #output of gapsRun in parallel
 		names(A)=names(Asd)<-paste(rep("Set",nSets),rep(1:nSets),sep="")
 		return(list("A"=A,"Asd"=Asd,"P"=P))
 	}
-	if(match=="A"){
+
+	if(match=="A")
+	{
 		A<-do.call(cbind,lapply(AP, function(x) x$Amean))
 		colnames(A)<-paste(rep(1:nSets,each=nFactor),rep(1:nFactor,nSets),sep=".")
 		P<-lapply(AP, function(x) x$Pmean)
