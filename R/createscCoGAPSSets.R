bad9c925c1afc0fba8d94f4a5373ea9eda095fbb
Genevieve Stein-O'Brien <genesofeve@vert.local>
2018-02-27 15:42:09 -0500

    createscCoGAPSsets

---
+++ R/createscCoGAPSSets.R
@@ -0,0 +1,48 @@
+#' Create Gene Sets for scCoGAPS
+#'
+#' @details factors whole genome data into randomly generated sets for indexing
+#'
+#' @param D data matrix
+#' @param S uncertainty matrix
+#' @param nSets number of sets to partition the data into
+#' @param simulationName name used to identify files created by this simulation
+#' @param samplingRatio vector of relative quantities to use for sampling celltypes
+#' @param annotionObj vector of same length as number of columns of D 
+#' @return simulationName used to identify saved files
+#' @examples
+#' data(SimpSim)
+#' createscCoGAPSSets(SimpSim.D, SimpSim.S, nSets=2, "example")
+#' @export
+createscCoGAPSSets <- function(D, S, nSets, simulationName,samplingRatio=NULL)
+{
+    # check gene names
+    if (length(unique(colnames(D))) != length(colnames(D)))
+    {
+        warning("Cell identifiers not unique!")
+    }
+
+    # partition data by sampling random sets of cells
+    cells <- 1:ncol(D)
+    setSize <- floor(length(cells) / nSets)
+    for (set in 1:nSets)
+    {
+        
+        if(is.null(samplingRatio)){
+        # sample cell names
+            sampleSize <- ifelse(set == nSets, length(cells), setSize)
+            cellset <- sample(cells, sampleSize, replace=FALSE)
+            cells <- cells[!(cells %in% cellset)]
+        } else {
+        if(length(unique(annotionObj))!=length(samplingRatio)){warning("Not all celltypes will be sampled from.")}
+        ct.indx<-lapply(unique(annotionObj),function(x) which(annotionObj == x))
+        cellset<-sample(colnames(D)[ct.indx[[x]]], samplingRatio[x],replace=TRUE)
+        }
+
+        # partition data
+        sampleD <- D[,cellset]
+        sampleS <- S[,cellset]
+        save(sampleD, sampleS, file=paste(simulationName, "_partition_", set,
+            ".RData", sep=""));
+    }
+    return(simulationName)
+}
