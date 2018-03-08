9d9df7f3d2958a6c834077db90131dda6c04cec1
jacobcvt12 <JCarey@telligen.com>
2018-03-08 16:36:05 -0500

    Add more functionality to unit test

--- tests/testthat/test_fixedgrowth.R
+++ tests/testthat/test_fixedgrowth.R
@@ -46,17 +46,49 @@ P <- patts$P
 A <- patts$A
 T <- patts$T
 
+rhos <- function(theta, D, A, P, T) {
+    # allocate curve (assume same number of recordings for all conditions
+
+    n <- ncol(P)
+    P_old_curve = logistic.growth(T, k=theta[1])
+    P_prime_curve = logistic.growth(T, k=theta[2])
+
+    Pnorm = sum(P_prime_curve)
+    Pnorm_old = sum(P_old_curve)
+
+    P_prime = P
+    A_prime = A
+
+    P_prime[3, ] = P_prime_curve / Pnorm
+    A_prime[, 3] = A_prime[, 3] * Pnorm / Pnorm_old
+
+    D_prime = A_prime %*% P_prime
+    D_orig = A %*% P
+
+    diff = D - D_prime
+    rho = norm(diff, type="2")
+
+    orig = D - D_orig
+    rho_thresh = norm(orig, type="2")
+
+    return(c(rho, rho_thresh))
+}
+
+#rhos(results[1, 1:2], D, A, P, T)
+
 # ABC - MCMC
 # one parameter, larger delta
+devtools::load_all()
+
 set.seed(20)
 iters <- 5000
 
-delta <- 1
-prior.mean <- 4
+delta <- 2
+prior.mean <- 2
 prior.sd <- 2
-epsilon <- 0.01
+epsilon <- 0.1
 
-devtools::load_all()
+# start at prior mean
 
 # test with fixed proposal
 results <- FixedMatrix(D, A, P, T,
@@ -65,17 +97,18 @@ results <- FixedMatrix(D, A, P, T,
                        TRUE)
 
 head(results)
-quantile(results[1, ], c(0.025, 0.5, 0.975))
 accepted <- results[, 3] < results[, 4]
+quantile(results[accepted, 1], c(0.025, 0.5, 0.975))
 mean(accepted)
 
 # test with MH proposal
 results <- FixedMatrix(D, A, P, T,
                        iters, delta, epsilon,
                        prior.mean, prior.sd,
-                       TRUE)
+                       FALSE)
 
 head(results)
-quantile(results[1, ], c(0.025, 0.5, 0.975))
 accepted <- results[, 3] < results[, 4]
+quantile(results[accepted, 1], c(0.025, 0.5, 0.975))
 mean(accepted)
+
