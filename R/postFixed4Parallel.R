d6132b67834239c47ccd25754be4d1a008f5b62a
Genevieve Stein-O'Brien <genesofeve@vert.local>
2018-04-02 09:46:46 -0400

    added postFixed4Parallel methods for fixed As

--- R/postFixed4Parallel.R
+++ R/postFixed4Parallel.R
@@ -5,23 +5,46 @@
 #' for gapsMapRun
 #' @return list of two data.frames containing the A matrix estimates or their
 #' corresponding standard deviations from output of parallel CoGAPS
-postFixed4Parallel <- function(AP.fixed, setPs)
+postFixed4Parallel <- function(AP.fixed, setValues, setMatrix="P")
 {
-    ASummary <- do.call(rbind,lapply(AP.fixed, function(x) x$Amean))
-    Asd <- do.call(rbind,lapply(AP.fixed, function(x) x$Asd))
-    #PSummary <- do.call(rbind,lapply(AP.fixed, function(x) x$Pmean))
-    PSummary <- AP.fixed[[1]]$Pmean
+    if(setMatrix=="P"){
+        ASummary <- do.call(rbind,lapply(AP.fixed, function(x) x$Amean))
+        Asd <- do.call(rbind,lapply(AP.fixed, function(x) x$Asd))
+        #PSummary <- do.call(rbind,lapply(AP.fixed, function(x) x$Pmean))
+        PSummary <- AP.fixed[[1]]$Pmean
 
-    Pmax <- apply(PSummary,1,max)
-    Pneu <- sweep(PSummary,1,Pmax,FUN="/")
-    Aneu <- sweep(ASummary,2,Pmax,FUN="*")
+        Pmax <- apply(PSummary,1,max)
+        Pneu <- sweep(PSummary,1,Pmax,FUN="/")
+        Aneu <- sweep(ASummary,2,Pmax,FUN="*")
 
-    X <- apply(Pneu,1,range)
-    Y <- apply(setPs,1,range)
-    colnames(X) <- colnames(Y)
-    if (all.equal(X,Y,tolerance=0.01) != TRUE)
-        warning("Patterns do not match fixed values.")
+        X <- apply(Pneu,1,range)
+        Y <- apply(setPs,1,range)
+        colnames(X) <- colnames(Y)
+        if (all.equal(X,Y,tolerance=0.01) != TRUE)
+            warning("Patterns do not match fixed values.")
 
-    As4fixPs<-list("A"=Aneu,"Asd"=Asd)
-    return(As4fixPs)
+        As4fixPs<-list("A"=Aneu,"Asd"=Asd)
+        return(As4fixPs)
+    } else if(setMatrix=="A"){
+        PSummary <- do.call(cbind,lapply(AP.fixed, function(x) x$Pmean))
+        Psd <- do.call(cbind,lapply(AP.fixed, function(x) x$Psd))
+        #PSummary <- do.call(rbind,lapply(AP.fixed, function(x) x$Pmean))
+        ASummary <- AP.fixed[[1]]$Amean
+
+        Amax <- apply(ASummary,1,max)
+        Aneu <- sweep(ASummary,1,Pmax,FUN="/")
+        Pneu <- sweep(PSummary,2,Pmax,FUN="*")
+
+        X <- apply(Aneu,1,range)
+        Y <- apply(setAs,1,range)
+        colnames(X) <- colnames(Y)
+        if (all.equal(X,Y,tolerance=0.01) != TRUE)
+            warning("As do not match fixed values.")
+
+        Ps4fixAs<-list("P"=Pneu,"Psd"=Psd)
+        return(Ps4fixAs)
+    } else{
+        warning("setMatrix can only take values of 'A' or 'P'")
+    }
+
 }
