f57956975ed5192b9ac98b1c430032e08c09a83f
Tom Sherman <tomsherman159@gmail.com>
2018-04-30 12:54:23 -0400

    temp print statement for debugging

--- src/cpp_tests/testGapsRunner.cpp
+++ src/cpp_tests/testGapsRunner.cpp
@@ -8,9 +8,9 @@ TEST_CASE("Test Top Level CoGAPS Call")
     Rcpp::NumericMatrix D = pkgEnv.find("SimpSim.D");
     Rcpp::NumericMatrix S = pkgEnv.find("SimpSim.S");
 
-    GapsRunner runner(D, S, 3, 500, 100, 500, 0, 0, 0.01f, 0.01f, 100.f, 100.f,
-        123, false, false, 0, "gaps_checkpoint.out", "N",
-        Rcpp::NumericMatrix());
+    //GapsRunner runner(D, S, 3, 500, 100, 500, 0, 0, 0.01f, 0.01f, 100.f, 100.f,
+    //    123, false, false, 0, "gaps_checkpoint.out", "N",
+    //    Rcpp::NumericMatrix());
     //CATCH_REQUIRE_NO_THROWS(runner.run());
 }
 
