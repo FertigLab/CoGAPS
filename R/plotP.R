6787d47b5f510eecca9506fae31080804d07e885
Tom Sherman <tomsherman159@gmail.com>
2018-06-03 20:00:32 -0400

    fixed plotP

--- R/plotP.R
+++ R/plotP.R
@@ -8,24 +8,20 @@
 #' data(SimpSim)
 #' plotP(SimpSim.result$Pmean, SimpSim.result$Psd)
 #' @export
-plotP <- function(Pmean, Psd)
+plotP <- function(Pmean, Psd=NULL)
 {
-    Nfactor <- nrow(Pmean)
-    Nobs <- ncol(Pmean)
-    RowP <- 1:Nobs
-    colors <- rainbow(Nfactor)
-    ylimits <- c(0,(max(Pmean + Psd)*1.05))
+    colors <- rainbow(nrow(Pmean))
+    xlimits <- c(0, ncol(Pmean) + 1)
+    ylimits <- c(0, (max(Pmean) * 1.05))
 
-    plotCI(x=RowP, y=Pmean[1,], col=colors[1],uiw=Psd[1,],
-        ylim=ylimits,type='l',ylab="Relative Amplitude")
+    plot(NULL, xlim=xlimits, ylim=ylimits, ylab="Relative Amplitude")
 
-    for (i in 2:Nfactor)
+    for (i in 1:nrow(Pmean))
     {
-        points(RowP, Pmean[i,], col=colors[i], pch=i)
-        plotCI(RowP, y=Pmean[i,], col=colors[i], uiw=Psd[i,],
-            add=TRUE)
+        lines(x=1:ncol(Pmean), y=Pmean[i,], col=colors[i])
+        points(x=1:ncol(Pmean), y=Pmean[i,], col=colors[i], pch=i)
     }
-
-    legend("bottom", paste("Pattern", 1:Nfactor, sep = ""),
-        pch = 1:Nfactor, lty=1,cex=0.8, col=colors,bty="y",ncol=5)
+    
+    legend("bottom", paste("Pattern", 1:nrow(Pmean), sep = ""),
+        pch = 1:nrow(Pmean), lty=1, cex=0.8, col=colors, bty="y", ncol=5)
 }
