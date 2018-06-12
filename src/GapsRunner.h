856ff5d3beba36ac642f1d1fef8f157d42478d02
Tom Sherman <tomsherman159@gmail.com>
2018-06-12 16:32:34 -0400

    more interface ideas

--- src/GapsRunner.h
+++ src/GapsRunner.h
@@ -86,11 +86,8 @@ public:
 
     // construct from R object
     GapsRunner(const Rcpp::NumericMatrix &D, const Rcpp::NumericMatrix &S,
-        unsigned nFactor, unsigned nEquil, unsigned nCool, unsigned nSample,
-        unsigned nOutputs, unsigned nSnapshots, float alphaA, float alphaP,
-        float maxGibbsMassA, float maxGibbsMassP, uint32_t seed, bool messages,
-        bool singleCellRNASeq, unsigned cptInterval, const std::string &cptFile,
-        char whichMatrixFixed, const Rcpp::NumericMatrix &FP, unsigned nCores);
+        unsigned nFactor, float alphaA, float alphaP, float maxGibbsMassA,
+        float maxGibbsMassP, bool singleCellRNASeq
     
     // construct from checkpoint file
     GapsRunner(const Rcpp::NumericMatrix &D, const Rcpp::NumericMatrix &S,
