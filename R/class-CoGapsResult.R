7159d9c7a0ebf41b6288d8597313cbdeaa965e27
Hyejune Limb <hlimb1@jhu.edu>
2018-06-18 16:03:14 -0400

    added show and plot methods to CoGAPSResult

--- R/class-CoGapsResult.R
+++ R/class-CoGapsResult.R
@@ -21,3 +21,33 @@ setMethod("initialize", "CoGapsResult",
 
 # TODO(Hyejune, Tom) Override the plot function for CoGapsResult
 
+setMethod('show', signature('CoGAPSResult'),
+          function(object)
+          {
+            print(paste("Amean has", nrow(Amean), "rows and", ncol(Amean), "columns"))
+            print(paste("Pmean has", nrow(Pmean), "rows and", ncol(Pmean), "columns"))
+            
+          }
+)
+
+setMethod('plot', signature('CoGAPSResult'),
+          function(Pmean)
+          {
+            
+              colors <- rainbow(nrow(Pmean))
+              xlimits <- c(0, ncol(Pmean) + 1)
+              ylimits <- c(0, (max(Pmean) * 1.05))
+              
+              plot(NULL, xlim=xlimits, ylim=ylimits, ylab="Relative Amplitude")
+              
+              for (i in 1:nrow(Pmean))
+              {
+                lines(x=1:ncol(Pmean), y=Pmean[i,], col=colors[i])
+                points(x=1:ncol(Pmean), y=Pmean[i,], col=colors[i], pch=i)
+              }
+              
+              legend("bottom", paste("Pattern", 1:nrow(Pmean), sep = ""),
+                     pch = 1:nrow(Pmean), lty=1, cex=0.8, col=colors, bty="y", ncol=5)
+              
+          }
+)
