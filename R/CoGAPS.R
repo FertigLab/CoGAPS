dfb57de42039ca5fde73a5be1a58e19f5ad71be5
Tom Sherman <tomsherman159@gmail.com>
2018-07-26 15:44:56 -0400

    moved extra argument parser

--- R/CoGAPS.R
+++ R/CoGAPS.R
@@ -143,6 +143,67 @@ parseExtraParams <- function(allParams, extraParams)
 
 }
 
+#' @rdname parseOldParams-methods
+#' @aliases parseOldParams
+setMethod("parseOldParams", signature(object="CogapsParams"),
+function(object, oldArgs)
+{
+    helper <- function(arg, params, newArg)
+    {
+        if (!is.null(oldArgs[[arg]]))
+        {
+            warning(paste("parameter", arg, "is deprecated, it will still",
+                "work, but setting", newArg, "in the params object is the",
+                "preferred method"))
+            params <- setParam(params, newArg, oldArgs[[arg]])
+            oldArgs[[arg]] <- NULL
+        }            
+        return(params)
+    }
+
+    object <- helper("nFactor", object, "nPatterns")
+    object <- helper("nIter", object, "nIterations")
+    object <- helper("nEquil", object, "nIterations")
+    object <- helper("nSample", object, "nIterations")
+    object <- helper("nOutR", object, "outputFrequency")
+    object <- helper("nOutput", object, "outputFrequency")
+    object <- helper("maxGibbmassA", object, "maxGibbsMassA")
+    object <- helper("max_gibbmass_paraA", object, "maxGibbsMassA")
+    object <- helper("maxGibbmassP", object, "maxGibbsMassP")
+    object <- helper("max_gibbmass_paraP", object, "maxGibbsMassP")
+    object <- helper("singleCellRNASeq", object, "singleCell")
+
+    if (!is.null(oldArgs$nSnapshots) | !is.null(oldArgs$sampleSnapshots) | !is.null(oldArgs$numSnapshots))
+    {
+        warning("snapshots not currently supported in release build")
+        oldArgs$nSnapshots <- NULL
+        oldArgs$sampleSnapshots <- NULL
+        oldArgs$numSnapshots <- NULL
+    }
+    if (!is.null(oldArgs$fixedPatterns))
+        stop("pass fixed matrix in with 'fixedMatrix' argument")
+    if (!is.null(oldArgs$S))
+        stop("pass uncertainty matrix in with 'uncertainty', not 'S'")
+
+    return(object)
+})
+
+#' @rdname parseDirectParams-methods
+#' @aliases parseDirectParams
+setMethod("parseDirectParams", signature(object="CogapsParams"),
+function(object, args)
+{
+    for (s in slotNames(object))
+    {
+        if (!is.null(args[[s]]))
+        {
+            object <- setParam(object, s, args[[s]])
+        }
+    }
+    return(object)
+})
+
+
 #' Check that provided data is valid
 #'
 #' @param data data matrix
