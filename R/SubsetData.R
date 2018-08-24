44fea7d925be8fcdefa87d2830877bfee5e108a8
Tom Sherman <tomsherman159@gmail.com>
2018-08-24 11:39:49 -0400

    added error message

--- R/SubsetData.R
+++ R/SubsetData.R
@@ -18,6 +18,8 @@ sampleWithExplictSets <- function(allParams, total)
             allNames <- allParams$geneNames
         else
             allNames <- allParams$sampleNames
+        if (is.null(allNames))
+            stop("missing gene/sample names")
         return(lapply(allParams$gaps@explicitSets, function(set) which(allNames %in% set)))
     }
 }
