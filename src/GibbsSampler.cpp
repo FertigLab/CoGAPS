5633149389efea5f0d87537e0e39b177605b4d28
Tom Sherman <tomsherman159@gmail.com>
2018-08-28 18:06:58 -0400

    still consitent

--- src/GibbsSampler.cpp
+++ src/GibbsSampler.cpp
@@ -9,25 +9,10 @@ void AmplitudeGibbsSampler::sync(PatternGibbsSampler &sampler)
     mAPMatrix = sampler.mAPMatrix;
 }
 
-unsigned AmplitudeGibbsSampler::getRow(uint64_t pos) const
+void PatternGibbsSampler::sync(AmplitudeGibbsSampler &sampler)
 {
-    return pos / (mBinSize * mNumCols);
-}
-
-unsigned AmplitudeGibbsSampler::getCol(uint64_t pos) const
-{
-    return (pos / mBinSize) % mNumCols;
-}
-
-bool AmplitudeGibbsSampler::canUseGibbs(unsigned row, unsigned col) const // NOLINT(misc-unused-parameters)
-{
-    return !gaps::algo::isVectorZero(mOtherMatrix->colPtr(col),
-        mOtherMatrix->nRow());
-}
-
-bool AmplitudeGibbsSampler::canUseGibbs(unsigned r1, unsigned c1, unsigned r2, unsigned c2) const
-{
-    return canUseGibbs(r1, c1) || canUseGibbs(r2, c2);
+    mOtherMatrix = &(sampler.mMatrix);
+    mAPMatrix = sampler.mAPMatrix;
 }
 
 void AmplitudeGibbsSampler::updateAPMatrix(unsigned row, unsigned col, float delta)
@@ -35,6 +20,29 @@ void AmplitudeGibbsSampler::updateAPMatrix(unsigned row, unsigned col, float del
     const float *other = mOtherMatrix->colPtr(col);
     float *ap = mAPMatrix.rowPtr(row);
     unsigned size = mAPMatrix.nCol();
+
+    gaps::simd::packedFloat pOther, pAP;
+    gaps::simd::Index i(0);
+    gaps::simd::packedFloat pDelta(delta);
+    for (; i <= size - i.increment(); ++i)
+    {
+        pOther.load(other + i);
+        pAP.load(ap + i);
+        pAP += pDelta * pOther;
+        pAP.store(ap + i);
+    }
+
+    for (unsigned j = i.value(); j < size; ++j)
+    {
+        ap[j] += delta * other[j];
+    }
+}
+
+void PatternGibbsSampler::updateAPMatrix(unsigned row, unsigned col, float delta)
+{
+    const float *other = mOtherMatrix->colPtr(col);
+    float *ap = mAPMatrix.colPtr(row);
+    unsigned size = mAPMatrix.nRow();
 
     gaps::simd::packedFloat pOther, pAP;
     gaps::simd::Index i(0);
@@ -71,77 +79,6 @@ unsigned r2, unsigned c2)
     return alphaParameters(r1, c1) + alphaParameters(r2, c2);
 }
 
-float AmplitudeGibbsSampler::computeDeltaLL(unsigned row, unsigned col, float mass)
-{
-    return gaps::algo::deltaLL(mDMatrix.nCol(), mDMatrix.rowPtr(row),
-        mSMatrix.rowPtr(row), mAPMatrix.rowPtr(row), mOtherMatrix->colPtr(col),
-        mass);
-}
-
-float AmplitudeGibbsSampler::computeDeltaLL(unsigned r1, unsigned c1, float m1,
-unsigned r2, unsigned c2, float m2)
-{
-    if (r1 == r2)
-    {
-        return gaps::algo::deltaLL(mDMatrix.nCol(), mDMatrix.rowPtr(r1),
-            mSMatrix.rowPtr(r1), mAPMatrix.rowPtr(r1), mOtherMatrix->colPtr(c1),
-            m1, mOtherMatrix->colPtr(c2), m2);
-    }
-    return computeDeltaLL(r1, c1, m1) + computeDeltaLL(r2, c2, m2);
-}
-
-/********************* PatternGibbsSampler Implementation *********************/
-
-void PatternGibbsSampler::sync(AmplitudeGibbsSampler &sampler)
-{
-    mOtherMatrix = &(sampler.mMatrix);
-    mAPMatrix = sampler.mAPMatrix;
-}
-
-unsigned PatternGibbsSampler::getRow(uint64_t pos) const
-{
-    return pos / (mBinSize * mNumCols);
-}
-
-unsigned PatternGibbsSampler::getCol(uint64_t pos) const
-{
-    return (pos / mBinSize) % mNumCols;
-}
-
-bool PatternGibbsSampler::canUseGibbs(unsigned row, unsigned col) const // NOLINT(misc-unused-parameters)
-{
-    return !gaps::algo::isVectorZero(mOtherMatrix->colPtr(col),
-        mOtherMatrix->nRow());
-}
-
-bool PatternGibbsSampler::canUseGibbs(unsigned r1, unsigned c1, unsigned r2, unsigned c2) const
-{
-    return canUseGibbs(r1, c1) || canUseGibbs(r2, c2);
-}
-
-void PatternGibbsSampler::updateAPMatrix(unsigned row, unsigned col, float delta)
-{
-    const float *other = mOtherMatrix->colPtr(col);
-    float *ap = mAPMatrix.colPtr(row);
-    unsigned size = mAPMatrix.nRow();
-
-    gaps::simd::packedFloat pOther, pAP;
-    gaps::simd::Index i(0);
-    gaps::simd::packedFloat pDelta(delta);
-    for (; i <= size - i.increment(); ++i)
-    {
-        pOther.load(other + i);
-        pAP.load(ap + i);
-        pAP += pDelta * pOther;
-        pAP.store(ap + i);
-    }
-
-    for (unsigned j = i.value(); j < size; ++j)
-    {
-        ap[j] += delta * other[j];
-    }
-}
-
 AlphaParameters PatternGibbsSampler::alphaParameters(unsigned row, unsigned col)
 {
     return gaps::algo::alphaParameters(mDMatrix.nRow(), mDMatrix.colPtr(row),
@@ -160,11 +97,28 @@ unsigned r2, unsigned c2)
     return alphaParameters(r1, c1) + alphaParameters(r2, c2);
 }
 
+float AmplitudeGibbsSampler::computeDeltaLL(unsigned row, unsigned col, float mass)
+{
+    AlphaParameters alpha = impl()->alphaParameters(row, col);
+    return mass * (alpha.su - alpha.s * mass / 2.f);
+}
+
+float AmplitudeGibbsSampler::computeDeltaLL(unsigned r1, unsigned c1, float m1,
+unsigned r2, unsigned c2, float m2)
+{
+    if (r1 == r2)
+    {
+        return gaps::algo::deltaLL(mDMatrix.nCol(), mDMatrix.rowPtr(r1),
+            mSMatrix.rowPtr(r1), mAPMatrix.rowPtr(r1), mOtherMatrix->colPtr(c1),
+            m1, mOtherMatrix->colPtr(c2), m2);
+    }
+    return computeDeltaLL(r1, c1, m1) + computeDeltaLL(r2, c2, m2);
+}
+
 float PatternGibbsSampler::computeDeltaLL(unsigned row, unsigned col, float mass)
 {
-    return gaps::algo::deltaLL(mDMatrix.nRow(), mDMatrix.colPtr(row),
-        mSMatrix.colPtr(row), mAPMatrix.colPtr(row), mOtherMatrix->colPtr(col),
-        mass);
+    AlphaParameters alpha = impl()->alphaParameters(row, col);
+    return mass * (alpha.su - alpha.s * mass / 2.f);
 }
 
 float PatternGibbsSampler::computeDeltaLL(unsigned r1, unsigned c1, float m1,
