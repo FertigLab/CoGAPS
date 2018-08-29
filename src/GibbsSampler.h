42349a05904701ef8eed3e9e23ba8d4df1e1cd36
Tom Sherman <tomsherman159@gmail.com>
2018-08-29 12:39:52 -0400

    express deltaLL in terms of alpha parameters

--- src/GibbsSampler.h
+++ src/GibbsSampler.h
@@ -6,74 +6,11 @@
 #include "data_structures/Matrix.h"
 #include "math/Algorithms.h"
 #include "math/Random.h"
-#include "math/SIMD.h"
-#include "utils/Archive.h"
-#include "utils/GapsAssert.h"
 
-#include <algorithm>
+#include <vector>
 
 class GibbsSampler
 {
-private:
-
-    friend class GapsStatistics;
-
-    ColMatrix mDMatrix;
-    ColMatrix mSMatrix;
-    ColMatrix mAPMatrix;
-
-    ColMatrix mMatrix;
-    const ColMatrix* mOtherMatrix;
-
-    GapsRng mPropRng;
-    AtomicDomain mDomain;
-
-    float mAlpha;
-    float mLambda;
-    float mMaxGibbsMass;
-    float mAnnealingTemp;
-    
-    unsigned mNumRows;
-    unsigned mNumCols;
-
-    uint64_t mNumBins;
-    uint64_t mBinSize;
-    uint64_t mDomainLength;
-
-    float mAvgQueue;
-    float mNumQueues;
-
-    void makeAndProcessProposal();
-    float deathProb(uint64_t nAtoms) const;
-
-    void birth();
-    void death();
-    void move();
-    void exchange();
-
-    unsigned getRow(uint64_t pos) const;
-    unsigned getCol(uint64_t pos) const;
-
-    bool canUseGibbs(unsigned col) const;
-    bool canUseGibbs(unsigned c1, unsigned c2) const;
-
-    void updateAPMatrix(unsigned row, unsigned col, float delta);
-
-    AlphaParameters alphaParameters(unsigned row, unsigned col);
-    AlphaParameters alphaParameters(unsigned r1, unsigned c1, unsigned r2,
-        unsigned c2);
-
-    float computeDeltaLL(unsigned row, unsigned col, float mass);
-    float computeDeltaLL(unsigned r1, unsigned c1, float m1, unsigned r2,
-        unsigned c2, float m2);
-
-    bool updateAtomMass(Atom *atom, float delta);
-    void acceptExchange(AtomicProposal *prop, float d1, unsigned r1,
-        unsigned c1, unsigned r2, unsigned c2);
-
-    OptionalFloat gibbsMass(AlphaParameters alpha, GapsRng *rng);
-    OptionalFloat gibbsMass(AlphaParameters alpha, float m1, float m2, GapsRng *rng);
-
 public:
 
     template <class DataType>
@@ -83,30 +20,80 @@ public:
     template <class DataType>
     void setUncertainty(const DataType &unc, bool transposeData,
         bool partitionRows, const std::vector<unsigned> &indices);
+
+    unsigned dataRows() const;
+    unsigned dataCols() const;
     
     void setSparsity(float alpha, bool singleCell);
     void setMaxGibbsMass(float max);
     void setAnnealingTemp(float temp);
-
     void setMatrix(const Matrix &mat);
-
-    void update(unsigned nSteps, unsigned nCores);
-
-    unsigned dataRows() const;
-    unsigned dataCols() const;
 
     float chi2() const;
     uint64_t nAtoms() const;
 
-    void sync(GibbsSampler &sampler);
+    void sync(const GibbsSampler &sampler);
+    void update(unsigned nSteps, unsigned nCores);
+
+    // serialization
+    friend Archive& operator<<(Archive &ar, GibbsSampler &sampler);
+    friend Archive& operator>>(Archive &ar, GibbsSampler &sampler);
 
     #ifdef GAPS_DEBUG
     bool internallyConsistent();
     #endif
 
-    // serialization
-    friend Archive& operator<<(Archive &ar, GibbsSampler &sampler);
-    friend Archive& operator>>(Archive &ar, GibbsSampler &sampler);
+private:
+
+    friend class GapsStatistics;
+
+    ColMatrix mDMatrix; // samples by genes for A, genes by samples for P
+    ColMatrix mSMatrix; // same configuration as D
+    ColMatrix mAPMatrix; // cached product of A and P, same configuration as D
+
+    ColMatrix mMatrix; // genes by patterns for A, samples by patterns for P
+    const ColMatrix *mOtherMatrix; // pointer to P if this is A, and vice versa
+
+    GapsRng mPropRng;
+    AtomicDomain mDomain; // data structure providing access to atoms
+
+    float mAlpha;
+    float mLambda;
+    float mMaxGibbsMass;
+    float mAnnealingTemp;
+    
+    unsigned mNumPatterns;
+    uint64_t mNumBins;
+    uint64_t mBinSize;
+    uint64_t mDomainLength;
+
+    void makeAndProcessProposal();
+    float deathProb(uint64_t nAtoms) const;
+
+    void birth();
+    void death();
+    void move();
+    void exchange();
+
+    void acceptExchange(Atom *a1, Atom *a2, float d1, unsigned r1,
+        unsigned c1, unsigned r2, unsigned c2);
+    bool updateAtomMass(Atom *atom, float delta);
+
+    OptionalFloat gibbsMass(AlphaParameters alpha, GapsRng *rng);
+    OptionalFloat gibbsMass(AlphaParameters alpha, float m1, float m2, GapsRng *rng);
+
+    void safelyChangeMatrix(unsigned row, unsigned col, float delta);
+    void updateAPMatrix(unsigned row, unsigned col, float delta);
+
+    unsigned getRow(uint64_t pos) const;
+    unsigned getCol(uint64_t pos) const;
+
+    bool canUseGibbs(unsigned col) const;
+    bool canUseGibbs(unsigned c1, unsigned c2) const;
+
+    AlphaParameters alphaParameters(unsigned row, unsigned col);
+    AlphaParameters alphaParameters(unsigned r1, unsigned c1, unsigned r2,
+        unsigned c2);
 };
 
 template <class DataType>
@@ -123,10 +110,7 @@ mDomain(mMatrix.nRow() * mMatrix.nCol()),
 mLambda(0.f),
 mMaxGibbsMass(100.f),
 mAnnealingTemp(1.f),
-mNumRows(mMatrix.nRow()),
-mNumCols(mMatrix.nCol()),
-mAvgQueue(0.f),
-mNumQueues(0.f),
+mNumPatterns(mMatrix.nCol()),
 mNumBins(mMatrix.nRow() * mMatrix.nCol()),
 mBinSize(std::numeric_limits<uint64_t>::max() / mNumBins),
 mDomainLength(mBinSize * mNumBins)
