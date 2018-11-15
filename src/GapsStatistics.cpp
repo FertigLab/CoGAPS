604d4e4fbab843a1546af287ec07b28a8324aeff
Tom Sherman <tomsherman159@gmail.com>
2018-11-15 14:59:48 -0500

    remove unneccesary copy

--- src/GapsStatistics.cpp
+++ src/GapsStatistics.cpp
@@ -52,19 +52,17 @@ Matrix GapsStatistics::Psd() const
 
 float GapsStatistics::meanChiSq(const DenseGibbsSampler &PSampler) const
 {
-    Matrix A(mAMeanMatrix / static_cast<float>(mStatUpdates));
-    Matrix P(mPMeanMatrix / static_cast<float>(mStatUpdates));
-    
     float chisq = 0.f;
     for (unsigned i = 0; i < PSampler.mDMatrix.nRow(); ++i)
     {
         for (unsigned j = 0; j < PSampler.mDMatrix.nCol(); ++j)
         {
             float m = 0.f;
-            for (unsigned k = 0; k < A.nCol(); ++k)
+            for (unsigned k = 0; k < mAMeanMatrix.nCol(); ++k)
             {
-                m += A(i,k) * P(j,k);
+                m += mAMeanMatrix(i,k) * mPMeanMatrix(j,k);
             }
+            m /= GAPS_SQ(static_cast<float>(mStatUpdates));
             chisq += GAPS_SQ((PSampler.mDMatrix(i,j) - m) / PSampler.mSMatrix(i,j));
         }
     }
@@ -73,19 +71,18 @@ float GapsStatistics::meanChiSq(const DenseGibbsSampler &PSampler) const
 
 float GapsStatistics::meanChiSq(const SparseGibbsSampler &PSampler) const
 {
-    Matrix A(mAMeanMatrix / static_cast<float>(mStatUpdates));
-    Matrix P(mPMeanMatrix / static_cast<float>(mStatUpdates));
-    
     float chisq = 0.f;
     for (unsigned i = 0; i < PSampler.mDMatrix.nRow(); ++i)
     {
         for (unsigned j = 0; j < PSampler.mDMatrix.nCol(); ++j)
         {
             float m = 0.f;
-            for (unsigned k = 0; k < A.nCol(); ++k)
+            for (unsigned k = 0; k < mAMeanMatrix.nCol(); ++k)
             {
-                m += A(i,k) * P(j,k);
+                m += mAMeanMatrix(i,k) * mPMeanMatrix(j,k);
             }
+            m /= GAPS_SQ(static_cast<float>(mStatUpdates));
+
             float d = PSampler.mDMatrix.getCol(j).at(i);
             float s = gaps::max(d * 0.1f, 0.1f);
             chisq += GAPS_SQ((d - m) / s);
