aea9a18b65435ae4a0af83e9fe13382a607bd340
Tom Sherman <tomsherman159@gmail.com>
2019-04-04 13:14:44 -0400

    use different seeds when testing checkpoint consistency

--- tests/testthat/test_checkpoints.R
+++ tests/testthat/test_checkpoints.R
@@ -5,13 +5,11 @@ test_that("Checkpoint System",
     if (CoGAPS::checkpointsEnabled())
     {
         data(GIST)
-        run1 <- CoGAPS(GIST.matrix, checkpointInterval=51,
+        run1 <- CoGAPS(GIST.matrix, checkpointInterval=51, seed=22,
             checkpointOutFile="test.out", messages=FALSE, nIterations=100)
         run2 <- CoGAPS(GIST.matrix, checkpointInFile="test.out", messages=FALSE,
-            nIterations=100)
+            nIterations=100, seed=33)
         file.remove("test.out")
-
-        print(max(run1@featureLoadings - run2@featureLoadings))
 
         expect_true(all.equal(run1@featureLoadings, run2@featureLoadings))
         expect_true(all.equal(run1@sampleFactors, run2@sampleFactors))
