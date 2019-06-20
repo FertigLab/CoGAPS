80631a76c641fc2f6ec382657ecc4fb491a5513f
sherman5 <tomsherman159@gmail.com>
2019-06-20 16:47:12 -0400

    remove implementation from header now that it's in its own file

--- src/file_parser/MatrixElement.h
+++ src/file_parser/MatrixElement.h
@@ -1,16 +1,7 @@
 #ifndef __COGAPS_MATRIX_ELEMENT_H__
 #define __COGAPS_MATRIX_ELEMENT_H__
 
-#include "../utils/GapsAssert.h"
-#include "../utils/GapsPrint.h"
-
-#include <sstream>
 #include <string>
-
-inline bool containsNumber(const std::string &s)
-{
-    return !s.empty() && s.find_first_not_of("0123456789.-") == std::string::npos;
-}
 
 struct MatrixElement
 {
@@ -18,21 +9,8 @@ struct MatrixElement
     unsigned col;
     float value;
 
-    MatrixElement(unsigned r, unsigned c, float v) // NOLINT
-        : row(r), col(c), value(v)
-    {}
-
-    MatrixElement(unsigned r, unsigned c, const std::string &s) // NOLINT
-        :  row(r), col(c), value(0.f)
-    {
-        std::stringstream ss(s);
-        ss >> value;
-        if (!containsNumber(s))
-        {
-            gaps_printf("\nError: Invalid entry found in input data: %s\n", s.c_str());
-            gaps_stop();
-        }
-    }
+    MatrixElement(unsigned r, unsigned c, float v);
+    MatrixElement(unsigned r, unsigned c, const std::string &s);
 };
 
-#endif
+#endif // __COGAPS_MATRIX_ELEMENT_H__
