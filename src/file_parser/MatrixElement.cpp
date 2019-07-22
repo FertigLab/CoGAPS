6f0451d5cffc5602bbec6239c9525f45277272c3
Tom Sherman <tomsherman159@gmail.com>
2019-07-22 17:37:24 -0400

    Added handler for scientific notation in input
    Previously, files that contained numbers like "3.45e-2" would fail
    with an error, but now this simple case is handled.

--- src/file_parser/MatrixElement.cpp
+++ src/file_parser/MatrixElement.cpp
@@ -6,20 +6,45 @@
 #include <sstream>
 #include <string>
 
-static bool containsNumber(const std::string &s)
+static bool isNumber(const std::string &s)
 {
     return !s.empty() && s.find_first_not_of("0123456789.-") == std::string::npos;
 }
 
-MatrixElement::MatrixElement(unsigned r, unsigned c, const std::string &s) // NOLINT
-    :
-row(r), col(c), value(0.f)
+static float processValue(const std::string &s)
 {
-    std::stringstream ss(s);
-    ss >> value;
-    if (!containsNumber(s))
+    if (isNumber(s)) // most common case should be the fastest
+    {
+        std::stringstream ss(s);
+        float tempValue;
+        ss >> tempValue;
+        return tempValue;
+    }
+    std::size_t pos = s.find("e"); // scientific notation only reason isNumber should fail
+    if (pos == std::string::npos)
     {
         gaps_printf("\nError: Invalid entry found in input data: %s\n", s.c_str());
         gaps_stop();
     }
+    else // handle scientific notation
+    {
+        std::string sBase = s.substr(0, pos);
+        std::string sExp = s.substr(pos + 1);
+        if (!isNumber(sBase) || !isNumber(sExp))
+        {
+            gaps_printf("\nError: Invalid entry found in input data: %s\n", s.c_str());
+            gaps_stop();
+        }
+        std::stringstream ssExp(sExp);
+        std::stringstream ssBase(sBase);
+        float fBase;
+        float fExp;
+        ssBase >> fBase;
+        ssExp >> fExp;
+        return fBase * std::pow(10.f, fExp);
+    }
 }
+
+MatrixElement::MatrixElement(unsigned r, unsigned c, const std::string &s) // NOLINT
+    : row(r), col(c), value(processValue(s))
+{}
