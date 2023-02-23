14306e87cd2f6f5557e514437ad83e775565584a
jeanettejohnson <johnsonje197@gmail.com>
2023-02-23 17:14:55 -0500

    wip

--- R/methods-CogapsResult.R
+++ R/methods-CogapsResult.R
@@ -162,7 +162,7 @@ plotPatternHallmarks <- function(patternhallmarks, whichpattern=1,...) {
 # NEW VIS FROM PDAC VIGNETTE
 #' @rdname plotPatternUMAP-methods
 #' @aliases plotPatternUMAP
-setMethod("plotPatternUMAP", signature(object="CogapsResult", cds="monocle3"),
+setMethod("plotPatternUMAP", signature(object="CogapsResult", cds),
 plotPatternUMAP <- function(object, cds){
   patMat <- object@sampleFactors
   # Create temporary CDS object
