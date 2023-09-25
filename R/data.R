df9c51913cc2482ce7fbf92b602bfaf501a477ca
Dmitrijs Lvovs <dmitrijs.lvovs@gmail.com>
2023-09-25 17:28:54 -0400

    add data definition file

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Toy example to illustrate CoGAPS
+#'
+#' A sample dataset to run CoGAPS on. For example containing 
+#'
+#' \itemize{
+#'   \item V1..V20. some variables, for example levels of gene expression
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name modsimdata
+#' @usage data(modsimdata)
+#' @format 'data.frame':	25 obs. of  20 variables.
+NULL
