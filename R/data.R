7da88d50304ad9a13f659a6da37f3fe1a134ec7e
Dmitrijs Lvovs <dmitrijs.lvovs@gmail.com>
2023-09-25 19:35:55 -0400

    convert modsimresult to rda

--- R/data.R
+++ R/data.R
@@ -1,6 +1,5 @@
-#' Toy example to illustrate CoGAPS
+#' Toy example to run CoGAPS on.
 #'
-#' A sample dataset to run CoGAPS on. For example containing 
 #'
 #' \itemize{
 #'   \item V1..V20. some variables, for example levels of gene expression
@@ -11,4 +10,14 @@
 #' @name modsimdata
 #' @usage data(modsimdata)
 #' @format 'data.frame':	25 obs. of  20 variables.
+NULL
+
+#' Result of applying CoGAPS on the Toy example.
+#' 
+#'
+#' @docType data
+#' @keywords datasets
+#' @name modsimresult
+#' @usage data(modsimresult)
+#' @format S4 class ‘CogapsResult’ [package “CoGAPS”] with 7 slots.
 NULL
