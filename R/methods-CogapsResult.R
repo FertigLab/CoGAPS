b30138240f931649c061f39b6d6affaba7d265d2
dimalvovs <dmitrijs.lvovs@gmail.com>
2024-05-09 16:53:10 -0400

    make original patternMarkers work in object setting

--- R/methods-CogapsResult.R
+++ R/methods-CogapsResult.R
@@ -389,66 +389,53 @@ function(patterngeneset, whichpattern=1, padj_threshold = 0.05)
   return(pl)
 })
 
-#' @param Amatrix A matrix of genes by weights resulting from CoGAPS or other NMF decomposition
-#' @param scaledPmatrix logical indicating whether the corresponding pattern matrix was fixed to have max 1 during decomposition
-#' @param Pmatrix the corresponding Pmatrix (patterns X samples) for the provided Amatrix (genes x patterns). This must be supplied if scaledPmatrix is FALSE.
-#' @param threshold the type of threshold to be used. The default "cut" will thresholding by the first gene to have a lower ranking, i.e. better fit to, a pattern. Alternatively, threshold="all" will return all of the genes in rank order for each pattern.
-#' @param lp a vector of weights for each pattern to be used for finding markers. If NA markers for each pattern of the A matrix will be used.
-#' @param full logical indicating whether to return the ranks of each gene for each pattern
-#'
+
 #' @return By default a non-overlapping list of genes associated with each \code{lp}. If \code{full=TRUE} a data.frame of
 #' genes rankings with a column for each \code{lp} will also be returned.
 #' @rdname patternMarkers-methods
 #' @aliases patternMarkers
 setMethod("patternMarkers", signature(object="CogapsResult"),
-function(
-    Amatrix=NA, #A matrix of genes by weights resulting from CoGAPS or other NMF decomposition
-    scaledPmatrix=FALSE, # logical indicating whether the corresponding pattern matrix was fixed to have max 1 during decomposition
-    Pmatrix=NA, #the corresponding Pmatrix (patterns X samples) for the provided Amatrix (genes x patterns). This must be supplied if scaledPmatrix is FALSE.
-    threshold="cut", # the type of threshold to be used. The default "cut" will thresholding by the first gene to have a lower ranking, i.e. better fit to, a pattern. Alternatively, threshold="all" will return all of the genes in rank order for each pattern.
-    lp=NA, # a vector of weights for each pattern to be used for finding markers. If NA markers for each pattern of the A matrix will be used.
-    full=FALSE #logical indicating whether to return the ranks of each gene for each pattern.
-){
+function(object, threshold, lp){
+    Amatrix <- object@featureLoadings
+    Pmatrix <- t(object@sampleFactors)
 
+    pscale <- apply(Pmatrix,1,max)   # rescale p's to have max 1
+    Amatrix <- sweep(Amatrix, 2, pscale, FUN="*")   # rescale A in accordance with p's having max 1
 
-if(scaledPmatrix==FALSE){
-    if(!is.na(Pmatrix)){
-      pscale <- apply(Pmatrix,1,max)   # rescale p's to have max 1
-      Amatrix <- sweep(Amatrix, 2, pscale, FUN="*")   # rescale A in accordance with p's having max 1
-  }
-    else(warning("P values must be provided if not already scaled"))
-  }
-# find the A with the highest magnitude
-Arowmax <- t(apply(Amatrix, 1, function(x) x/max(x)))
-pmax<-apply(Amatrix, 1, max)
-# determine which genes are most associated with each pattern
-ssranks<-matrix(NA, nrow=nrow(Amatrix), ncol=ncol(Amatrix),dimnames=dimnames(Amatrix))#list()
-ssgenes<-matrix(NA, nrow=nrow(Amatrix), ncol=ncol(Amatrix),dimnames=NULL)
-nP=dim(Amatrix)[2]
-if(!is.na(lp)){
-    if(length(lp)!=dim(Amatrix)[2]){
-        warning("lp length must equal the number of columns of the Amatrix")
+    # find the A with the highest magnitude
+    Arowmax <- t(apply(Amatrix, 1, function(x) x/max(x)))
+
+    # determine which genes are most associated with each pattern
+    ssranks<-matrix(NA, nrow=nrow(Amatrix), ncol=ncol(Amatrix),dimnames=dimnames(Amatrix))#list()
+    ssgenes<-matrix(NA, nrow=nrow(Amatrix), ncol=ncol(Amatrix),dimnames=NULL)
+    nP=dim(Amatrix)[2]
+    if(!is.na(lp)){
+        if(length(lp)!=dim(Amatrix)[2]){
+            warning("lp length must equal the number of columns of the Amatrix")
+        }
+            sstat <- apply(Arowmax, 1, function(x) sqrt(t(x-lp)%*%(x-lp)))
+            ssranks[order(sstat),i] <- 1:length(sstat)
+            ssgenes[,i]<-names(sort(sstat,decreasing=FALSE))
+    } else {for(i in 1:nP){
+            lp <- rep(0,dim(Amatrix)[2])
+            lp[i] <- 1
+            sstat <- apply(Arowmax, 1, function(x) sqrt(t(x-lp)%*%(x-lp)))
+            ssranks[order(sstat),i] <- 1:length(sstat)
+            ssgenes[,i]<-names(sort(sstat,decreasing=FALSE))
+    }}
+    if(threshold=="cut"){
+            pIndx<-apply(ssranks,1,which.min)
+            pNames <- sort(unique(pIndx))
+            ssgenes.th <- lapply(pNames,function(x) names(pIndx[pIndx==x]))
+            names(ssgenes.th) <- paste0("Pattern_",pNames)
     }
-        sstat <- apply(Arowmax, 1, function(x) sqrt(t(x-lp)%*%(x-lp)))
-        ssranks[order(sstat),i] <- 1:length(sstat)
-        ssgenes[,i]<-names(sort(sstat,decreasing=FALSE))
-} else {for(i in 1:nP){
-        lp <- rep(0,dim(Amatrix)[2])
-        lp[i] <- 1
-        sstat <- apply(Arowmax, 1, function(x) sqrt(t(x-lp)%*%(x-lp)))
-        ssranks[order(sstat),i] <- 1:length(sstat)
-        ssgenes[,i]<-names(sort(sstat,decreasing=FALSE))
-}}
-if(threshold=="cut"){
-        pIndx<-apply(ssranks,1,which.min)
-        ssgenes.th <- lapply(unique(pIndx),function(x) names(pIndx[pIndx==x]))
-}
-if(threshold=="All"){
-        geneThresh <- apply(sweep(ssranks,1,t(apply(ssranks, 1, min)),"-"),2,function(x) which(x==0))
-        ssgenes.th <- lapply(geneThresh,names)
-}
-if(full){return(list("PatternMarkers"=ssgenes.th,"PatternRanks"=ssranks))
-} else{return("PatternMarkers"=ssgenes.th)}
+    if(threshold=="all"){
+            geneThresh <- apply(sweep(ssranks,1,t(apply(ssranks, 1, min)),"-"),2,function(x) which(x==0))
+            ssgenes.th <- lapply(geneThresh,names)
+    }
+    
+    return(list("PatternMarkers"=ssgenes.th,"PatternRanks"=ssranks))
+
 })
 
 #' @rdname calcCoGAPSStat-methods
--- tests/testthat/test_patternMarkers.R
+++ tests/testthat/test_patternMarkers.R
@@ -8,20 +8,12 @@ test_that("patternMarkers work with threshold = 'all' for sparse mode", {
     
 })
 
-test_that("patternMarkers work with threshold = 'all' and axis = 2", {
+test_that("patternMarkers work with threshold = 'all'", {
     #set up
     data(GIST)
     res <- CoGAPS(GIST.data_frame, nIterations=100,
                   seed=1, messages=FALSE)
-    expect_no_error(patternMarkers(res, threshold = "all", axis = 2))
-})
-
-test_that("patternMarkers work with threshold = 'all' and axis = 1", {
-    #set up
-    data(GIST)
-    res <- CoGAPS(GIST.data_frame, nIterations=100,
-                  seed=1, messages=FALSE)
-    expect_no_error(patternMarkers(res, threshold = "all", axis = 1))
+    expect_no_error(patternMarkers(res, threshold = "all"))
 })
 
 test_that("no empty patternMarkers and their names in threshold = all", {
