22fac53a2e9cff4fd173196c913a279a78100647
dimalvovs <dmitrijs.lvovs@gmail.com>
2024-06-05 10:59:27 -0400

    add test that checks that all genes are returned for threshold="all"

--- tests/testthat/test_patternMarkers.R
+++ tests/testthat/test_patternMarkers.R
@@ -16,16 +16,20 @@ test_that("patternMarkers work with threshold = 'all'", {
     expect_no_error(patternMarkers(res, threshold = "all"))
 })
 
-test_that("no empty patternMarkers and their names in threshold = all", {
+test_that("test outputs generic in threshold = all", {
     #set up
     data(GIST)
     res <- CoGAPS(GIST.data_frame, nIterations=100,
                   seed=1, messages=FALSE)
     test <- patternMarkers(res, threshold = "all")
     marker_lengths <- sapply(test$PatternMarkers, length)
+    #not all patterns have no markers
     expect_equal(sum(marker_lengths > 0), length(marker_lengths))
+    #all markers are non-empty
     expect_equal(sum(nchar(names(test$PatternMarkers)) > 0),
                  length(names(test$PatternMarkers)))
+    expect_equal(length(unlist(test$PatternMarkers)),
+                 nrow(res@featureLoadings))
 })
 
 
@@ -74,8 +78,9 @@ test_that("genes are sorted by by their ranks in the output", {
                         c(3,3,10,3,3),
                         c(4,4,4,10,4),
                         c(5,5,5,5,10)), nrow=5, ncol=5))
+  test_mat <- rbind(test_mat,diag(1, 5, 5)[,5:1])
   mock <- list(
-    featureLoadings = rbind(test_mat,diag(1, 5, 5)[,5:1]),
+    featureLoadings = test_mat,
     sampleFactors = matrix(rep(1, 25), nrow = 5), # 5x5 matrix of 1s
     factorStdDev = matrix(runif(25), nrow = 5), # 5x5 matrix of random numbers
     meanChiSq = runif(1), # single random number
