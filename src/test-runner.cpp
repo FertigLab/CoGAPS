23e99e00ab50f4eff7031da0dc2258834101e316
dimalvovs <dmitrijs.lvovs@gmail.com>
2024-09-13 12:47:50 -0400

    unconditionally run cpp tests

--- src/test-runner.cpp
+++ src/test-runner.cpp
@@ -1,15 +1,12 @@
-#ifdef GAPS_CPP_UNIT_TESTS
-    #define CATCH_CONFIG_RUNNER
-    #include "cpp_tests/catch.h"
-#endif
+
+#define CATCH_CONFIG_RUNNER
+#include "cpp_tests/catch.h"
+
 
 // [[Rcpp::export]]
 int run_catch_unit_tests()
 {
-#ifdef GAPS_CPP_UNIT_TESTS
     Catch::Session session;
     int numFailed = session.run();
     return (numFailed < 0xFF ? numFailed : 0xFF);
-#endif
-    return 1;
 }
