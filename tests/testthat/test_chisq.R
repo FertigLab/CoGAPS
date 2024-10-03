640154413c3da9a4daa1cf2d0faf34642e6a649e
dimalvovs <dmitrijs.lvovs@gmail.com>
2024-10-03 13:03:36 -0400

    change tolerance for chisq diff to float precision * n elements

--- tests/testthat/test_chisq.R
+++ tests/testthat/test_chisq.R
@@ -11,21 +11,7 @@ test_that('chi-square reported by CoGAPS mathches manually calculated (w/uncerta
     M <- A %*% t(P)
 
     calculated <- sum(((data - M)/unc)^2)
-    expect_equal(reported, calculated, tolerance = 1e-6)
-})
-
-
-test_that('chi-square reported by CoGAPS mathches manually calculated (w/o uncertainty)',{
-    data(GIST)
-    data <- GIST.data_frame
-    res <- CoGAPS(data, nIterations=1000,
-                  seed=1, messages=FALSE, sparseOptimization=FALSE)
-    res_unc <- CoGAPS(data, nIterations=1000, uncertainty = 0.1*as.matrix(data),
-                  seed=1, messages=FALSE, sparseOptimization=FALSE)
-
-    reported <- getMeanChiSq(res)
-    reported_unc <- getMeanChiSq(res_unc)
-    message('unc. not provided:', reported, '\nunc. provided:', reported_unc)
-    #we do not know how big of a difference is allowed, setting to too big
-    expect_equal(reported, reported_unc, tolerance = 1e+6)
+    #Set the tolerance to float precision times matrix size
+    sprintf("difference: %.17f", abs(reported - calculated))
+    expect_equal(reported, calculated, tolerance = (1e-7)*prod(dim(data)))
 })
