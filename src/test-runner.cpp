12fb26f1197aaefe11e7b7effc260b181adc4588
dimalvovs <dmitrijs.lvovs@gmail.com>
2024-10-03 17:36:57 -0400

    Revert "unconditionally run cpp tests"
    This reverts commit 23e99e00ab50f4eff7031da0dc2258834101e316.

--- src/test-runner.cpp
+++ src/test-runner.cpp
@@ -1,12 +1,15 @@
-
-#define CATCH_CONFIG_RUNNER
-#include "cpp_tests/catch.h"
-
+#ifdef GAPS_CPP_UNIT_TESTS
+    #define CATCH_CONFIG_RUNNER
+    #include "cpp_tests/catch.h"
+#endif
 
 // [[Rcpp::export]]
 int run_catch_unit_tests()
 {
+#ifdef GAPS_CPP_UNIT_TESTS
     Catch::Session session;
     int numFailed = session.run();
     return (numFailed < 0xFF ? numFailed : 0xFF);
+#endif
+    return 1;
 }
