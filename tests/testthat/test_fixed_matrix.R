2edb7a38334cbd5e70db5b55dc39a0ff74191364
dimalvovs <dmitrijs.lvovs@gmail.com>
2024-11-11 16:55:01 -0600

    test that the fixed A is returned in result

--- tests/testthat/test_fixed_matrix.R
+++ tests/testthat/test_fixed_matrix.R
@@ -25,3 +25,24 @@ test_that("Fixing P matrix works", {
   expect_true(abs(sum((res1@featureLoadings - res2@featureLoadings))) <
               abs(sum((res1@featureLoadings - res3@featureLoadings))))
 })
+
+
+test_that("Fixed A matrix is returned in resulting object",{
+    data("GIST")
+    testMatrix <- GIST.matrix
+    nPat <- 5
+    nIter <- 100
+    #run CoGAPS normally
+    res1 <- CoGAPS(testMatrix, nPatterns=nPat, nIterations=nIter, seed=42,
+                     messages = FALSE)
+
+    #run with fixed A matrix to reconstruct P
+    fixedA <- res1@featureLoadings
+    param <- CogapsParams()
+    param <- setFixedPatterns(param, fixedA, "A")
+    res2 <- CoGAPS(testMatrix, param, nPatterns=nPat, nIterations=nIter, seed=42,
+                   messages = FALSE)
+
+    #expect that the fixed A is returned back in the resulting object
+    expect_true(all.equal(res2@featureLoadings, fixedA))
+})
