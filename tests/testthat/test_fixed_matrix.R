798bb246f1e58e7da82d45203e849c75ab68de57
dimalvovs <dmitrijs.lvovs@gmail.com>
2024-11-12 17:46:42 -0600

    update fixed matrix test to accept all 0s

--- tests/testthat/test_fixed_matrix.R
+++ tests/testthat/test_fixed_matrix.R
@@ -24,25 +24,37 @@ test_that("Fixing P matrix works", {
   #expect that the A with real fixed P is better than A with random fixed P
   expect_true(abs(sum((res1@featureLoadings - res2@featureLoadings))) <
               abs(sum((res1@featureLoadings - res3@featureLoadings))))
+  #expect all 0s in the fixed P matrix
+  expect_equal(unique(min(res3@sampleFactors), max(res3@sampleFactors)), 0)
 })
 
+testthat::test_that("Fixing A matrix works", {
+  data("GIST")
+  testMatrix <- GIST.matrix
+  nPat <- 5
+  nIter <- 100
+  #run CoGAPS normally
+  res1 <- CoGAPS(testMatrix, nPatterns=nPat, nIterations=nIter, seed=42,
+                 messages = FALSE)
 
-test_that("Fixed A matrix is returned in resulting object",{
-    data("GIST")
-    testMatrix <- GIST.matrix
-    nPat <- 5
-    nIter <- 100
-    #run CoGAPS normally
-    res1 <- CoGAPS(testMatrix, nPatterns=nPat, nIterations=nIter, seed=42,
-                     messages = FALSE)
+  #run with fixed A matrix to reconstruct P
+  fixedA <- res1@featureLoadings
+  param <- CogapsParams()
+  param <- setFixedPatterns(param, fixedA, "A")
+  res2 <- CoGAPS(testMatrix, param, nPatterns=nPat, nIterations=nIter, seed=42,
+                 messages = FALSE)
 
-    #run with fixed A matrix to reconstruct P
-    fixedA <- res1@featureLoadings
-    param <- CogapsParams()
-    param <- setFixedPatterns(param, fixedA, "A")
-    res2 <- CoGAPS(testMatrix, param, nPatterns=nPat, nIterations=nIter, seed=42,
-                   messages = FALSE)
+  #run with random fixedA
+  fixedA <- matrix(runif(nrow(testMatrix) * nPat, 1, 10), ncol=nPat)
+  param <- CogapsParams()
+  param <- setFixedPatterns(param, fixedA, "A")
+  res3 <- CoGAPS(testMatrix, param, nPatterns=nPat, nIterations=nIter, seed=42,
+                 messages = FALSE)
 
-    #expect that the fixed A is returned back in the resulting object
-    expect_true(all.equal(res2@featureLoadings, fixedA))
+  #expect that the P with real fixed A is better than P with random fixed A
+  expect_true(abs(sum((res1@sampleFactors - res2@sampleFactors))) <
+              abs(sum((res1@sampleFactors - res3@sampleFactors)))
+  )
+  #expect all 0s in the fixed A matrix
+  expect_equal(unique(min(res3@featureLoadings), max(res3@featureLoadings)), 0)
 })
