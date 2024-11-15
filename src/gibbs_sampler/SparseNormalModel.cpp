eb66b77a7a8939a4b9ec1618a0b5ef49d080cd59
dimalvovs <dmitrijs.lvovs@gmail.com>
2024-11-15 10:36:09 -0500

    add comments to sparse chisq calculation

--- src/gibbs_sampler/SparseNormalModel.cpp
+++ src/gibbs_sampler/SparseNormalModel.cpp
@@ -36,28 +36,58 @@ void SparseNormalModel::extraInitialization()
     // nop - not needed
 }
 
+
 float SparseNormalModel::chiSq() const
 {
     float chisq = 0.f;
-    for (unsigned j = 0; j < mDMatrix.nCol(); ++j)
+    for (unsigned j = 0; j < mDMatrix.nCol(); ++j)                                          // for each col of Data Matrix
     {
-        for (unsigned i = 0; i < mDMatrix.nRow(); ++i)
+        for (unsigned i = 0; i < mDMatrix.nRow(); ++i)                                      // for each row of Data Matrix
         {
-            float dot = gaps::dot(mMatrix.getRow(j), mOtherMatrix->getRow(i));
-            chisq += dot * dot;
+            float dot = gaps::dot(mMatrix.getRow(j), mOtherMatrix->getRow(i));              // dot = A * P
+            chisq += dot * dot;                                                             // chisq = chisq+dot^2
         }
 
-        SparseIterator<1> it(mDMatrix.getCol(j));
+        SparseIterator<1> it(mDMatrix.getCol(j));                                           // iterate non-zero elements of col j
         while (!it.atEnd())
         {
-            float dot = gaps::dot(mMatrix.getRow(j), mOtherMatrix->getRow(it.getIndex()));
-            float dsq = get<1>(it) * get<1>(it);
-            chisq += 1 + dot * (dot - 2 * get<1>(it) - dsq * dot) / dsq;
+            float dot = gaps::dot(mMatrix.getRow(j), mOtherMatrix->getRow(it.getIndex()));  // dot = A * P again
+            float dsq = get<1>(it) * get<1>(it);                                            // dsq = Data squared  
+            chisq += 1 + dot * (dot - 2 * get<1>(it) - dsq * dot) / dsq;                    // chisq
             it.next();
         }
     }
-    return chisq * mBeta;
+    return chisq * mBeta;                                                                   // mBeta is 100
 }
+
+
+// // test version - mfo@jh.edu
+// float SparseNormalModel::chiSq() const
+// {
+//     double chisq = 0.f;
+//     double mDM = 0.f;
+//     double mAP = 0.f;
+//     double mSM = 0.f;
+//     double add = 0.f;
+//     for (unsigned i = 0; i < mDMatrix.nRow(); ++i)
+//     {
+//         for (unsigned j = 0; j < mDMatrix.nCol(); ++j)
+//         {
+//             GAPS_ASSERT(mSMatrix(i,j) > 0.f);
+//             mDM = mDMatrix(i,j);
+//             mAP = mAPMatrix(i,j);
+//             mSM = mSMatrix(i,j);
+//  //           if (mDM < 0.00001f) { mDM = 0.f;}
+//  //           if (mAP < 0.00001f) { mAP = 0.f;}
+//             add = ((mDM - mAP) / mSM) * ((mDM - mAP) / mSM);
+//             chisq += add;
+//         }
+//     }
+//     gaps_printf("%f",chisq);
+//     gaps_printf("\n");
+//     return chisq;
+// }
+
 
 float SparseNormalModel::dataSparsity() const
 {
