4f0fae3bc53330b666a5d0484d049c257ccd8be3
Sasha Favorov <favorov@sensi.org>
2025-01-05 00:52:06 -0500

    test for A and P columns to be nonzero

---
+++ tests/testthat/test_non_zero_A_and_P.R
@@ -0,0 +1,17 @@
+context("CoGAPS")
+
+
+test_that("CoGAPS return non-zero-populated patterns and loadings",
+{
+    data(GIST)
+    testDataFrame <- GIST.data_frame
+    nPat<-5
+    res <<- CoGAPS(testDataFrame, nPatterns=nPat, nIterations=100, outputFrequency=50, seed=42, messages=TRUE)
+    expect_equal(nrow(res@featureLoadings), 1363)
+    expect_equal(ncol(res@featureLoadings), nPat)
+    expect_equal(nrow(res@sampleFactors), 9)
+    expect_equal(ncol(res@sampleFactors), nPat)
+    expect_true(all(apply(res@sampleFactors,2,sum) != 0))
+    expect_true(all(apply(res@featureLoadings,2,sum) != 0))
+})
+
