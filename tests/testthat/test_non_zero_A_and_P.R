f883fe813a0d48ccc42a95a0b040e6d2cf950771
Sasha Favorov <favorov@sensi.org>
2025-01-05 16:40:51 -0500

    editing the test title, and no more global vars

--- tests/testthat/test_non_zero_A_and_P.R
+++ tests/testthat/test_non_zero_A_and_P.R
@@ -1,12 +1,12 @@
 context("CoGAPS")
 
 
-test_that("CoGAPS return non-zero-populated patterns and loadings",
+test_that("CoGAPS returns non-zero-populated patterns and loadings",
 {
     data(GIST)
     testDataFrame <- GIST.data_frame
     nPat<-5
-    res <<- CoGAPS(testDataFrame, nPatterns=nPat, nIterations=100, outputFrequency=50, seed=42, messages=TRUE)
+    res <- CoGAPS(testDataFrame, nPatterns=nPat, nIterations=100, outputFrequency=50, seed=42, messages=TRUE)
     expect_equal(nrow(res@featureLoadings), 1363)
     expect_equal(ncol(res@featureLoadings), nPat)
     expect_equal(nrow(res@sampleFactors), 9)
