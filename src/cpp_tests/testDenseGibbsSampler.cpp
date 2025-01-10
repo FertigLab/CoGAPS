12e7c53d6b2ac7ce4d6d88d05d177ea26054b41a
Sasha Favorov <favorov@sensi.org>
2025-01-10 15:09:09 -0500

    Added gist matrix part, going on

--- src/cpp_tests/testDenseGibbsSampler.cpp
+++ src/cpp_tests/testDenseGibbsSampler.cpp
@@ -2,11 +2,31 @@
 #include "../testthat-tweak.h"
 #include "../gibbs_sampler/SingleThreadedGibbsSampler.h"
 #include "../gibbs_sampler/DenseNormalModel.h"
+#include "../data_structures/Matrix.h"
 
-TEST_CASE("Test DenseGibbsSampler","[densesinglesample]")
+#include <Rcpp.h>
+
+// convert R to C++ data type
+// it is copied from CoGAPS.cpp not to change the maun headers
+static Matrix convertRMatrix(const Rcpp::NumericMatrix &rmat)
 {
-    SECTION("Construct from data matrix")
+    Matrix mat(rmat.nrow(), rmat.ncol());
+    for (unsigned i = 0; i < mat.nRow(); ++i)
     {
+        for (unsigned j = 0; j < mat.nCol(); ++j)
+        {
+            mat(i,j) = rmat(i,j);
+        }
+    }
+    return mat;
+}
+
+
+TEST_CASE("Test DenseGibbsSampler on random matrix","[densesinglesampler][randommat]")
+{
+    SECTION("Construct from random data matrix")
+    {
+        
         Matrix data(25, 50);
         for (unsigned i = 0; i < data.nRow(); ++i)
         {
@@ -16,23 +36,70 @@ TEST_CASE("Test DenseGibbsSampler","[densesinglesample]")
             }
         }
 
-        GapsRandomState randState(123);
+        GapsRandomState randState(42);
+        GapsParameters params(data);
+        SingleThreadedGibbsSampler<DenseNormalModel> ASampler(data, true, false, params.alphaA,
+            params.maxGibbsMassA, params, &randState);
+        SingleThreadedGibbsSampler<DenseNormalModel> PSampler(data, false, false, params.alphaP,
+            params.maxGibbsMassP, params, &randState);
+        
+
+        REQUIRE(ASampler.chiSq() == 100.f * data.nRow() * data.nCol());
+        REQUIRE(PSampler.chiSq() == 100.f * data.nRow() * data.nCol());
+    
+        double AChiInit=ASampler.chiSq();
+        double PChiInit=PSampler.chiSq();
+
+        ASampler.sync(PSampler);
+        PSampler.sync(ASampler);
+        ASampler.extraInitialization();
+        PSampler.extraInitialization();
+
+        REQUIRE(ASampler.chiSq() == AChiInit);
+        REQUIRE(PSampler.chiSq() == PChiInit);
+
+    #ifdef GAPS_DEBUG
+        REQUIRE(ASampler.internallyConsistent());
+        REQUIRE(PSampler.internallyConsistent());
+    #endif
+        ASampler.sync(PSampler);
+        PSampler.sync(ASampler);
+        ASampler.extraInitialization();
+        PSampler.extraInitialization();
+
+        REQUIRE(ASampler.chiSq() == AChiInit);
+        REQUIRE(PSampler.chiSq() == PChiInit);
+    }
+}
+
+TEST_CASE("Test DenseGibbsSampler on gist matrix","[densesinglesampler][gistmat]")
+{
+
+    SECTION("Construct from gist matrix")
+    {
+        Rcpp::Environment env = Rcpp::Environment::global_env();
+        Rcpp::Function load("data");
+        //R function is data()
+        load("GIST");
+        Matrix data=convertRMatrix(env["GIST.matrix"]);
+
+        GapsRandomState randState(42);
         GapsParameters params(data);
         SingleThreadedGibbsSampler<DenseNormalModel> ASampler(data, true, false, params.alphaA,
             params.maxGibbsMassA, params, &randState);
         SingleThreadedGibbsSampler<DenseNormalModel> PSampler(data, false, false, params.alphaP,
             params.maxGibbsMassP, params, &randState);
     
-        REQUIRE(ASampler.chiSq() == 100.f * data.nRow() * data.nCol());
-        REQUIRE(PSampler.chiSq() == 100.f * data.nRow() * data.nCol());
+        double AChiInit=ASampler.chiSq();
+        double PChiInit=PSampler.chiSq();
     
         ASampler.sync(PSampler);
         PSampler.sync(ASampler);
         ASampler.extraInitialization();
         PSampler.extraInitialization();
 
-        REQUIRE(ASampler.chiSq() == 100.f * data.nRow() * data.nCol());
-        REQUIRE(PSampler.chiSq() == 100.f * data.nRow() * data.nCol());
+        REQUIRE(ASampler.chiSq() == AChiInit);
+        REQUIRE(PSampler.chiSq() == PChiInit);
 
     #ifdef GAPS_DEBUG
         REQUIRE(ASampler.internallyConsistent());
