1b62c62ebf6d3ee664ab588b9a06d12a3ba121b8
Sasha Favorov <favorov@sensi.org>
2025-01-29 16:45:27 -0500

    endl brfore of birth output

--- src/gibbs_sampler/SingleThreadedGibbsSampler.h
+++ src/gibbs_sampler/SingleThreadedGibbsSampler.h
@@ -140,7 +140,7 @@ void SingleThreadedGibbsSampler<DataModel>::birth()
     unsigned row = (pos / mBinLength) / mNumPatterns;
     unsigned col = (pos / mBinLength) % mNumPatterns;
     
-    std::cout<<"Birth at ["<<row<<","<<col<<"]; apos="<<pos<<std::endl;
+    std::cout<<std::end<<l"Birth at ["<<row<<","<<col<<"]; apos="<<pos<<std::endl;
 
     // try to get mass using gibbs, resort to exponential if needed
     OptionalFloat mass = DataModel::canUseGibbs(col)
