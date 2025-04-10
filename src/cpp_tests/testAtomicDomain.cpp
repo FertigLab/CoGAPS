bd474fa668f994eb3fd9a8a97dab721dbc95ccab
Sasha Favorov <favorov@sensi.org>
2025-04-10 01:15:51 -0400

    added test of move and etase

--- src/cpp_tests/testAtomicDomain.cpp
+++ src/cpp_tests/testAtomicDomain.cpp
@@ -38,19 +38,50 @@ TEST_CASE("AtomicDomain populate","[atomicdomain][populate]")
 
 TEST_CASE("AtomicDomain move","[atomicdomain][move]")
 {
-    //SECTION("Populate")
-    //{
-        AtomicDomain domain(100);
-        domain.insert((uint64_t)200000,0.01); 
-        //atomic coord and mass
-        domain.insert((uint64_t)100000,0.02); 
-        //atomic coord and mass
-        domain.insert((uint64_t)400000,0.03); 
-        //atomic coord and mass
-        domain.insert((uint64_t)300000,0.04); 
-        //domain.move(a2p,30000);
-      //  REQUIRE(domain.size() == 4);
-    //}
+    AtomicDomain domain(100);
+    domain.insert((uint64_t)200000,0.01); 
+    //atomic coord and mass
+    domain.insert((uint64_t)100000,0.02); 
+    //atomic coord and mass
+    domain.insert((uint64_t)400000,0.03); 
+    //atomic coord and mass
+    domain.insert((uint64_t)300000,0.04); 
+    //domain.move(a2p,30000);
+    REQUIRE(domain.size() == 4);
+    
+    SECTION("Test move")
+    {
+        domain.move(domain.storedAtom(3),310000);
+        REQUIRE(domain.storedAtom(3)->pos() == 310000);
+        auto iter=domain.front_it();
+        REQUIRE(iter->first==100000);
+        REQUIRE(iter->second==1);
+        iter++;
+        REQUIRE(iter->first==200000);
+        REQUIRE(iter->second==0);
+        iter++;
+        REQUIRE(iter->first==310000);
+        REQUIRE(iter->second==3);
+        iter++;
+        REQUIRE(iter->first==400000);
+        REQUIRE(iter->second==2);
+    }
+    
+}
+
+TEST_CASE("AtomicDomain structure","[atomicdomain][structure]")
+{
+    AtomicDomain domain(100);
+    domain.insert((uint64_t)200000,0.01); 
+    //atomic coord and mass
+    domain.insert((uint64_t)100000,0.02); 
+    //atomic coord and mass
+    domain.insert((uint64_t)400000,0.03); 
+    //atomic coord and mass
+    domain.insert((uint64_t)300000,0.04); 
+    //domain.move(a2p,30000);
+    REQUIRE(domain.size() == 4);
+    
     SECTION("Test structure")
     {
         REQUIRE(domain.front()->pos() == 100000);
@@ -122,6 +153,68 @@ TEST_CASE("AtomicDomain move","[atomicdomain][move]")
     
 }
 
+
+TEST_CASE("AtomicDomain depopulate","[atomicdomain][depopulate]") {
+    AtomicDomain domain(100);
+    domain.insert((uint64_t)200000,0.01); 
+    //atomic coord and mass
+    domain.insert((uint64_t)100000,0.02); 
+    //atomic coord and mass
+    domain.insert((uint64_t)400000,0.03); 
+    //atomic coord and mass
+    domain.insert((uint64_t)300000,0.04); 
+    //domain.move(a2p,30000);
+    REQUIRE(domain.size() == 4);
+    SECTION("DePopulate")
+    {   
+        domain.erase(domain.storedAtom(0));
+        
+        REQUIRE(domain.front()->pos() == 100000);
+        REQUIRE(domain.front()->index() == 1);
+        REQUIRE(domain.front()->hasLeft() == false);
+        REQUIRE(domain.front()->hasRight() == true);
+        REQUIRE(domain.front()->rightIndex() == 0); 
+        
+        REQUIRE(domain.storedAtom(0)->pos() == 300000);
+        REQUIRE(domain.storedAtom(0)->index() == 0);
+        REQUIRE(domain.storedAtom(0)->hasLeft() == true);
+        REQUIRE(domain.storedAtom(0)->hasRight() == true);
+        REQUIRE(domain.storedAtom(0)->leftIndex() == 1);
+        REQUIRE(domain.storedAtom(0)->rightIndex() == 2);  
+        
+        REQUIRE(domain.storedAtom(1)->pos() == 100000);
+        REQUIRE(domain.storedAtom(1)->index() == 1);
+        REQUIRE(domain.storedAtom(1)->hasLeft() == false);
+        REQUIRE(domain.storedAtom(1)->hasRight() == true);
+        //REQUIRE(domain.storedAtom(1)->leftIndex() == 100);
+        REQUIRE(domain.storedAtom(1)->rightIndex() == 0);
+        
+        REQUIRE(domain.storedAtom(2)->pos() == 400000);
+        REQUIRE(domain.storedAtom(2)->index() == 2);
+        REQUIRE(domain.storedAtom(2)->hasLeft() == true);
+        REQUIRE(domain.storedAtom(2)->hasRight() == false);
+        REQUIRE(domain.storedAtom(2)->leftIndex() == 0);
+        //REQUIRE(domain.storedAtom(2)->rightIndex() == 100);
+    }
+    SECTION("Test map structure")
+    {
+        //in new section, we get a new copy of domain
+        //to be sure,
+        REQUIRE(domain.size()==4);
+        //erase
+        domain.erase(domain.storedAtom(0));
+        auto iter=domain.front_it();
+        REQUIRE(iter->first==100000);
+        REQUIRE(iter->second==1);
+        iter++;
+        REQUIRE(iter->first==300000);
+        REQUIRE(iter->second==0);
+        iter++;
+        REQUIRE(iter->first==400000);
+        REQUIRE(iter->second==2);
+    }
+}
+
 TEST_CASE("AtomicDomain randompick","[atomicdomain][randompick]") {
     SECTION("RandomAtomChoose")
     {
@@ -145,24 +238,7 @@ TEST_CASE("AtomicDomain randompick from empty","[atomicdomain][randompickempty]"
     }
 }
 
-TEST_CASE("AtomicDomain depopulate","[atomicdomain][depopulate]") {
-    SECTION("DePopulate")
-    {
-        AtomicDomain domain(2);
-        domain.insert((uint64_t)200000,0.02); 
-        //atomic coord and mass
-        domain.insert((uint64_t)100000,0.03); 
-        domain.insert((uint64_t)400000,0.01); 
-        size_t sz=domain.size();
-        Atom * del = domain.randomAtom(&AtomicRng);
-        std::cout<<"We remove at "<<del->pos()<<std::endl<<std::flush;
-        REQUIRE(domain.size() == sz);
-        domain.erase(del); 
-        //atomic coord
-        REQUIRE(domain.size() == sz-1);
-        std::cout<<"domain.size()"<<domain.size()<<std::endl<<std::flush;
-    }
-}
+
 
 TEST_CASE("AtomicDomain depopulateempty","[atomicdomain][depopulateempty]") {
     SECTION("DePopulate")
