ec5b7b6122ec698f4d7aea3884cea89e123b7de4
Alexander Favorov <favorov@sensi.org>
2025-05-07 00:58:48 -0400

    test is more verbose ans easy to understand

--- src/cpp_tests/static_cast_standalone_test.cpp
+++ src/cpp_tests/static_cast_standalone_test.cpp
@@ -1,30 +1,22 @@
 //compile: 
 //c++ static_cast_standalone_test.cpp -std=gnu++17 -O0
-//clang++ -arch x86_64 -std=gnu++17 -O0 static_cast_standalone_test.cpp
 #include <cstdint>
 #include <iostream>
 #include <limits>
 
-void priint(uint64_t a){
-    std::cout<<a<<std::endl;
-}
-
 int main(){
-    uint64_t uvalue64=std::numeric_limits<uint64_t>::max();
+    uint64_t uvalue64_m=std::numeric_limits<uint64_t>::max();
+    uint64_t uvalue64_s=uvalue64_m-10;
     //maximal is 18446744073709551615
     //effect holds for value 18446744073709551600
     //std::cout<<"Max uint64_t:\n"<<std::numeric_limits<uint64_t>::max()<<std::endl;
-    std::cout<<uvalue64<<std::endl;
-    double dvalue=static_cast<double>(uvalue64);
-    priint(dvalue);
-    std::cout<<dvalue<<std::endl;
-    std::cout<<static_cast<uint64_t>(dvalue)<<std::endl;
-    uint32_t uvalue32=std::numeric_limits<uint32_t>::max();
-    //maximal is 18446744073709551615
-    //effect holds for value 18446744073709551600
-    //std::cout<<"Max uint64_t:\n"<<std::numeric_limits<uint64_t>::max()<<std::endl;
-    std::cout<<uvalue32<<std::endl;
-    dvalue=static_cast<double>(uvalue32);
-    priint(dvalue);
+    std::cout<<"uint64_t value 1 (max) : "<<uvalue64_m<<std::endl;
+    std::cout<<"uint64_t value 2 (max-10) : "<<uvalue64_s<<std::endl;
+    double dvalue_m=static_cast<double>(uvalue64_m);
+    double dvalue_s=static_cast<double>(uvalue64_s);
+    std::cout<<"value 1 as double : "<<dvalue_m<<std::endl;
+    std::cout<<"value 2 as double : "<<dvalue_s<<std::endl;
+    std::cout<<"value 1 as uint64_t casted back from double : "<<static_cast<uint64_t>(dvalue_m)<<std::endl;
+    std::cout<<"value 2 as uint64_t casted back from double : "<<static_cast<uint64_t>(dvalue_s)<<std::endl;
     return 0;
 }
