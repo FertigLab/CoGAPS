2d7ee688c27e466497461a4f6159abd61bbccd90
dimalvovs <dmitrijs.lvovs@gmail.com>
2025-09-14 10:23:20 -0400

    add Distributed_CoGAPS tests

--- tests/testthat/test_DistributedCogaps.R
+++ tests/testthat/test_DistributedCogaps.R
@@ -8,13 +8,18 @@ test_that("featureLoadings and sampleFactors are not all 0s in single-cell", {
   params <- setDistributedParams(params, nSets = 2)
   data(GIST)
   cg <- CoGAPS(GIST.matrix, params=params)
-  
+
   featureLoadings <- cg@featureLoadings
   sampleFactors <- cg@sampleFactors
-  
-  # Check that featureLoadings and sampleFactors are not all zeros
+
+  # Check featureLoadings and sampleFactors: smaller dimension is nPatterns
+  # and larger dimension matches data dimensions
   expect_false(all(featureLoadings == 0))
   expect_false(all(sampleFactors == 0))
+  expect_true(sort((dim(sampleFactors)))[1] == params@nPatterns)
+  expect_true(sort((dim(featureLoadings)))[1] == params@nPatterns)
+  expect_true(sort((dim(sampleFactors)))[2] == ncol(GIST.matrix))
+  expect_true(sort((dim(featureLoadings)))[2] == nrow(GIST.matrix))
 })
 
 test_that("featureLoadings and sampleFactors are not all 0s in genome-wide", {
@@ -30,8 +35,13 @@ test_that("featureLoadings and sampleFactors are not all 0s in genome-wide", {
   
   featureLoadings <- cg@featureLoadings
   sampleFactors <- cg@sampleFactors
-  
-  # Check that featureLoadings and sampleFactors are not all zeros
+
+  # Check featureLoadings and sampleFactors: smaller dimension is nPatterns
+  # and larger dimension matches data dimensions
   expect_false(all(featureLoadings == 0))
   expect_false(all(sampleFactors == 0))
+  expect_true(sort((dim(sampleFactors)))[1] == params@nPatterns)
+  expect_true(sort((dim(featureLoadings)))[1] == params@nPatterns)
+  expect_true(sort((dim(sampleFactors)))[2] == ncol(GIST.matrix))
+  expect_true(sort((dim(featureLoadings)))[2] == nrow(GIST.matrix))
 })
